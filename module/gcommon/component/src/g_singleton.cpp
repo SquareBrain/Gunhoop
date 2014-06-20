c32c80b3cfc022ef47079b7db0bbda214d8f2e9a
duye <wangwei_cp@163.com>
2014-06-21 00:44:03 +0800

    add files

---
+++ module/gcommon/component/src/g_singleton.cpp
@@ -0,0 +1,21 @@
+/************************************************************************************
+**  
+*    @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		singleton.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-06-18
+* @note 
+*
+*  1. 2014-06-18 duye Created this file
+* 
+*/
+
+#include <singleton.h>
+
+G_NS_GCOMMON_BEG
+G_NS_GCOMMON_END
