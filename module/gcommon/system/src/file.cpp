04ccfd1c76ff7b2b8bf244e057adc1b27e6ca09d
duye <wangwei_cp@163.com>
2014-06-20 23:34:38 +0800

    modify file.cpp

--- module/gcommon/system/src/file.cpp
+++ module/gcommon/system/src/file.cpp
@@ -4,20 +4,20 @@
 *
 *************************************************************************************/
 /**
-* @file		duye_file.cpp
+* @file		file.cpp
 * @version     
 * @brief      
 * @author   duye
-* @date     2013-11-16
+* @date     2013-06-20
 * @note 
 *
-*  1. 2013-11-16 duye Created this file
+*  1. 2013-06-20 duye Created this file
 * 
 */
 
-#include <duye/posix/stdio/inc/duye_file.h>
+#include "file.h"
 
-DUYE_POSIX_NS_BEG
+G_NS_GCOMMON_BEG
 
 File::File() : m_fd(-1), m_flags(0)
 {
@@ -29,9 +29,9 @@ File::~File()
     Close();
 }
 
-D_Bool File::Create(const D_Int8* filePath)
+bool File::Create(const GInt8* filePath)
 {
-    D_Int32 fd = creat(filePath, D_CREATE_MODE);
+    GInt32 fd = creat(filePath, D_CREATE_MODE);
     if (fd != -1)
     {
         close(fd);
@@ -40,75 +40,75 @@ D_Bool File::Create(const D_Int8* filePath)
     return (fd != -1 ? true : false);
 }
 
-D_Bool File::OpenR(const D_Int8* filePath)
+bool File::OpenR(const GInt8* filePath)
 {
-    const D_Int32 flags = O_RDONLY;
+    const GInt32 flags = O_RDONLY;
     return OrgOpen(filePath, flags);
 }
 
-D_Bool File::OpenW(const D_Int8* filePath)
+bool File::OpenW(const GInt8* filePath)
 {
-    const D_Int32 flags = O_WRONLY;
+    const GInt32 flags = O_WRONLY;
     return OrgOpen(filePath, flags);
 }
 
-D_Bool File::OpenWA(const D_Int8* filePath)
+bool File::OpenWA(const GInt8* filePath)
 {
-    const D_Int32 flags = O_WRONLY | O_APPEND;
+    const GInt32 flags = O_WRONLY | O_APPEND;
     return OrgOpen(filePath, flags);
 }
 
-D_Bool File::OpenWC(const D_Int8* filePath)
+bool File::OpenWC(const GInt8* filePath)
 {
-    const D_Int32 flags = O_WRONLY | O_CREAT;
+    const GInt32 flags = O_WRONLY | O_CREAT;
     return OrgOpen(filePath, flags, D_CREATE_MODE);
 }
 
-D_Bool File::OpenWCA(const D_Int8* filePath)
+bool File::OpenWCA(const GInt8* filePath)
 {
-    const D_Int32 flags = O_WRONLY | O_CREAT | O_APPEND;
+    const GInt32 flags = O_WRONLY | O_CREAT | O_APPEND;
     return OrgOpen(filePath, flags, D_CREATE_MODE);
 }
 
-D_Bool File::OpenRW(const D_Int8* filePath)
+bool File::OpenRW(const GInt8* filePath)
 {
-    const D_Int32 flags = O_RDWR;
+    const GInt32 flags = O_RDWR;
     return OrgOpen(filePath, flags);
 }
 
-D_Bool File::OpenRWA(const D_Int8* filePath)
+bool File::OpenRWA(const GInt8* filePath)
 {
-    const D_Int32 flags = O_RDWR | O_APPEND;
+    const GInt32 flags = O_RDWR | O_APPEND;
     return OrgOpen(filePath, flags);
 }
 
-D_Bool File::OpenRWC(const D_Int8* filePath)
+bool File::OpenRWC(const GInt8* filePath)
 {
-    const D_Int32 flags = O_RDWR | O_CREAT;
+    const GInt32 flags = O_RDWR | O_CREAT;
     return OrgOpen(filePath, flags, D_CREATE_MODE);
 }
 
-D_Bool File::OpenRWCA(const D_Int8* filePath)
+bool File::OpenRWCA(const GInt8* filePath)
 {
-    const D_Int32 flags = O_RDWR | O_CREAT | O_APPEND;
+    const GInt32 flags = O_RDWR | O_CREAT | O_APPEND;
     return OrgOpen(filePath, flags, D_CREATE_MODE);
 }
 
-D_UInt64 File::Read(D_Int8* buffer, const D_UInt64 size)
+GUint64 File::Read(GInt8* buffer, const GUint64 size)
 {
     D_ASSERT(buffer != NULL && size > 0);
     return read(m_fd, buffer, size);
 }
 
-D_UInt64 File::Write(const D_Int8* buffer, const D_UInt64 size)
+GUint64 File::Write(const GInt8* buffer, const GUint64 size)
 {
     D_ASSERT(buffer != NULL && size > 0);
     return write(m_fd, buffer, size);
 }
 
-D_Bool File::Close()
+bool File::Close()
 {
-    D_Bool ret = true;
+    bool ret = true;
     if (m_fd == -1)
     {
         return ret;
@@ -122,7 +122,7 @@ D_Bool File::Close()
     return ret;
 }
 
-D_Bool File::OrgOpen(const D_Int8* filePath, const D_Int32 flags, const D_UInt32 mode)
+bool File::OrgOpen(const GInt8* filePath, const GInt32 flags, const GUint32 mode)
 {
     D_ASSERT(filePath != NULL);
     
@@ -131,7 +131,7 @@ D_Bool File::OrgOpen(const D_Int8* filePath, const D_Int32 flags, const D_UInt32
         return true;
     }
 
-    D_UInt32 pathLen = strlen(filePath);
+    GUint32 pathLen = strlen(filePath);
     if (pathLen >= D_PATH_MAX)
     {
         return false;
@@ -147,4 +147,4 @@ D_Bool File::OrgOpen(const D_Int8* filePath, const D_Int32 flags, const D_UInt32
     return (m_fd != -1 ? true : false);
 }
 
-DUYE_POSIX_NS_END
+G_NS_GCOMMON_END
