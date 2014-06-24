836dfeb17e3fbe823938a99a8928ec45045a502c
duye <wangwei_cp@163.com>
2014-06-24 19:09:08 +0800

    add folder

---
+++ module/gutils/logger/src/g_logger.cpp
@@ -0,0 +1,57 @@
+/***********************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+************************************************************************************/
+/**
+* @file		g_logger.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-06-18
+* @note 
+*
+*  1. 2014-06-18 duye Created this file
+* 
+*/
+#include <cstdio>
+#include <g_logger.h>
+
+G_NS_GCOMMON_BEG
+
+GResult Logger::Init()
+{
+    return true;
+}
+       
+GResult Logger::Uninit()
+{
+    return true;
+}
+      
+void Logger::PrintError(const GInt8* prefix, const GInt8* str)
+{
+    printf("<ERROR><%s>%s", prefix, str);
+}
+   
+void Logger::PrintWarn(const GInt8* prefix, const GInt8* str)
+{
+    printf("<WARN><%s>%s", prefix, str);
+}
+       
+void Logger::PrintInfo(const GInt8* prefix, const GInt8* str)
+{
+    printf("<INFO><%s>%s", prefix, str);
+}
+       
+void Logger::PrintDebug(const GInt8* prefix, const GInt8* str)
+{
+    printf("<DEBUG><%s>%s", prefix, str);
+}
+
+void Logger::PrintTrace(const GInt8* prefix, const GInt8* str)
+{
+    printf("<TRACE><%s>%s", prefix, str);
+}
+
+G_NS_GCOMMON_END
