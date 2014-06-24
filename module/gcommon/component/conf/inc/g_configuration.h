10d29ddeac70420c2815521f6264a7461024bc2a
nazij <yq5213976@163.com>
2014-06-24 22:03:30 +0800

    1. add uml folder; 2. add conf folder;

---
+++ module/gcommon/component/conf/inc/g_configuration.h
@@ -0,0 +1,23 @@
+/************************************************************************************
+**  
+* @copyright (c) 2010-2019, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file	    g_configuration.h
+* @version     
+* @brief      
+* @author   ylh
+* @date     2014-06-24
+* @note 
+*
+*  1. 2014-06-24 ylh Created this file
+* 
+*/
+
+#pragma once
+
+G_NS_GCOMMON_BEG
+
+G_NS_GCOMMON_END
+
