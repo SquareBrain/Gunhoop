fabeec4c4545957191424bb462e50e89df80b655
duye <wangwei_cp@163.com>
2014-06-24 22:34:52 +0800

    modify

--- module/gcommon/system/inc/g_condition.h
+++ module/gcommon/system/inc/g_condition.h
@@ -23,63 +23,73 @@
 
 G_NS_GCOMMON_BEG
 
-// brief : POSIX condition wrapper
-//	
-// usage :
-//	Condition myCond;
-//  
-//  void ThreadOne()
-//  {
-//      if (!myCond.Wait())
-//      {
-//          return;
-//      }
-//      
-//      // to do
-//  }
-//
-//  void ThreadTwo()
-//  {
-//      sleep(1);
-//      myCond.Signal();
-//  }
+/** 
+ * POSIX condition wrapper
+ * usage :
+ *  Condition myCond;
+ *
+ *  void ThreadOne()
+ *  {
+ *      if (!myCond.Wait())
+ *      {
+ *          return;
+ *      }
+ *    
+ *      // to do
+ *  }
+ *
+ *  void ThreadTwo()
+ *  {
+ *      sleep(1);
+ *      myCond.Signal();
+ *  }
+ */
 class Condition 
 {
 public:
 	Condition();
 	~Condition();
 
-	// brief : wake up the waitting thread, only wake up one thread,  
-	// according to the thread priority and waitting time
-	// @para
-	// @return true/false
-	// note:
+    /**
+     * wake up the waitting thread, only wake up one thread,
+     * according to the thread priority and waitting time.
+     * @return true/false
+     * @note 
+     */ 
 	bool Signal();
-	
-	// brief : wake up all waitting threads
-	// @para
-	// @return true/false
-	// note:
+	 
+    /**
+     * wake up all waitting threads
+     * @param [in] timeout waitting timeout
+     * @return true/false
+     * @note 
+     */		
 	bool Broadcast();
 	
-	// brief : let current thread has reached the awaited state, utill be wake up
-	// @para
-	// @return true/false
-	// note:
+    /**
+     * let current thread has reached the awaited state, utill be wake up
+     * @param [in] timeout waitting timeout
+     * @return true/false
+     * @note 
+     */		
 	bool Wait();
 	
-	// brief : let current thread has reached the awaited state, 
-	// utill be wake up or timeout return
-	// @para [in]timeout : waitting timeout
-	// @return true/false
-	// note:
+    /**
+     * let current thread has reached the awaited state,
+     * utill be wake up or timeout return
+     * @param [in] timeout waitting timeout
+     * @return true/false
+     * @note 
+     */	
 	bool Wait(const GUint32 timeout);
 
-private:
-	// brief : to prevent copying
-	// @para 
-	// @return 
-	// note:
+private: 
+    /**
+     * for to prevent copying
+     * @param [in] timeout waitting timeout
+     * @return true/false
+     * @note 
+     */		
 	Condition(const Condition&); 
 	void operator=(const Condition&);	
     
