4e40ef65c216082971ba94404bb0cf00df20d9d9
duye <wangwei_cp@163.com>
2014-06-26 21:43:20 +0800

    modify files

--- module/gutils/xml/inc/g_xml.h
+++ module/gutils/xml/inc/g_xml.h
@@ -4,13 +4,14 @@
 *
 ************************************************************************************/
 /**
-* @file		duye_xml.h
+* @file		g_xml.h
 * @version     
 * @brief      
 * @author   duye
 * @date     2013-11-15
 * @note 
 *
+*  2. 2014-06-24 duye Migration to Gohoop project
 *  1. 2013-11-15 duye Created this file
 * 
 */
@@ -20,4 +21,5 @@
 #include <duye/util/inc/duye_util_def.h>
 
 G_NS_GUTILS_BEG
+
 G_NS_END
--- module/gutils/xml/src/g_xml.cpp
+++ module/gutils/xml/src/g_xml.cpp
@@ -4,12 +4,14 @@
 *
 ************************************************************************************/
 /**
-* @file		duye_xml.h
+* @file		g_xml.h
 * @version     
 * @brief      
 * @author   duye
 * @date     2013-11-15
 * @note 
+*
+*  2. 2014-06-24 duye Migration to Gohoop project
 *
 *  1. 2013-11-15 duye Created this file
 * 
