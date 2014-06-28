035a038f612e1d54b263832a70bb9cb8289029ae
duye <wangwei_cp@163.com>
2014-06-28 16:12:47 +0800

    add files

--- module/gcommon/module.mk
+++ module/gcommon/module.mk
@@ -10,9 +10,9 @@ LIBS_PATH:=
 
 LIBS:= 
 
-GCOMMON_PATH:=$(GOHOOP_TOP)/gcommon
+MODULE_PATH:=$(GOHOOP_TOP)/gcommon
 SLIBS:= \
-	$(GCOMMON_PATH)/component/output/lib/libcomponent.a \
-	$(GCOMMON_PATH)/system/output/lib/libsystem.a \
+	$(MODULE_PATH)/component/output/lib/libcomponent.a \
+	$(MODULE_PATH)/system/output/lib/libsystem.a
 
 include ../shared.mk
---
+++ module/gdata/module.mk
@@ -0,0 +1,16 @@
+PRONAME:=gdata
+VERSION:=1.0.0
+TARGET:=lib$(PRONAME)
+PRE_DEFINED:= 
+BUILD_PATH:=$(shell pwd)
+
+INCLUDES:=
+
+LIBS_PATH:= 
+
+LIBS:= 
+
+MODULE_PATH:=$(GOHOOP_TOP)/gdata
+SLIBS:=
+
+include ../shared.mk
