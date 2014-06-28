c13de7f5514b4fbf90a579dc2564b5d1da828f17
duye <wangwei_cp@163.com>
2014-06-28 18:41:59 +0800

    modify files

---
+++ module/gcommon/module_static.mk
@@ -0,0 +1,18 @@
+PRONAME:=gcommon
+VERSION:=1.0.0
+TARGET:=lib$(PRONAME)
+PRE_DEFINED:= 
+BUILD_PATH:=$(shell pwd)
+
+INCLUDES:=
+
+LIBS_PATH:= 
+
+LIBS:= 
+
+MODULE_PATH:=$(GOHOOP_TOP)/gcommon
+SLIBS:= \
+	$(MODULE_PATH)/component/output/lib/libcomponent.a \
+	$(MODULE_PATH)/system/output/lib/libsystem.a
+
+include ../static.mk
