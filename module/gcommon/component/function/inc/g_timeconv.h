7c6829331829f9178f9f3cf06cb7673ec09d4c71
duye <wangwei_cp@163.com>
2014-07-02 07:36:01 +0800

    add files

---
+++ module/gcommon/component/function/inc/g_timeconv.h
@@ -0,0 +1,82 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_time.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-03-09
+* @note 
+*
+*  2. 2014-06-21 duye move to gohoop project 
+*  1. 2014-03-09 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <g_type.h>
+
+G_NS_GCOMMON_BEG
+
+/** 
+ * POSIX system time
+ */
+class TimeUtil
+{
+public:
+    /**
+     * get system time format:20140309
+	 * @param [in]sysTime : input system time with nanosecond
+	 * @param [out]sysTimeStr : output system time with nanosecond
+	 * @param [in] bufSize : output buffer size
+	 * @return true/false
+     * @note 
+     */			
+    static bool convTimeA(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize);  
+
+    /**
+     * get system time format:2014-03-09
+	 * @param [in]sysTime : input system time with nanosecond
+	 * @param [out]sysTimeStr : output system time with nanosecond
+	 * @param [in] bufSize : output buffer size
+	 * @return true/false
+     * @note 
+     */		
+    static bool convTimeB(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize); 
+ 
+    /**
+     * get system time format:2014/03/09
+	 * @param [in]sysTime : input system time with nanosecond
+	 * @param [out]sysTimeStr : output system time with nanosecond
+	 * @param [in] bufSize : output buffer size
+	 * @return true/false
+     * @note 
+     */		
+    static bool convTimeC(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize); 
+  
+    /**
+     * get system time format:2014.03.09
+	 * @param [in]sysTime : input system time with nanosecond
+	 * @param [out]sysTimeStr : output system time with nanosecond
+	 * @param [in] bufSize : output buffer size
+	 * @return true/false
+     * @note 
+     */		
+    static bool convTimeD(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize);  
+
+    /**
+     * get system time format:2014-03-09:999
+	 * @param [in]sysTime : input system time with nanosecond
+	 * @param [out]sysTimeStr : output system time with nanosecond
+	 * @param [in] bufSize : output buffer size
+	 * @return true/false
+     * @note 
+     */		
+    static bool convTimeE(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize);
+};
+
+G_NS_END
---
+++ module/gcommon/component/function/src/g_numconv.cpp
@@ -0,0 +1,91 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_time.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-03-09
+* @note 
+*
+*  1. 2014-03-09 duye Created this file
+* 
+*/
+#include <g_time.h>
+
+G_NS_GCOMMON_BEG
+
+bool TimeUtil::convTimeA(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize)
+{
+    if (outSysTime == NULL)
+    {
+        return false;
+    }
+    
+    const GInt8* curSysTime = "20140309";
+    
+    sprintf(outSysTime, "%s", curSysTime);
+
+    return true;
+}
+
+bool TimeUtil::convTimeB(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize)
+{
+    if (outSysTime == NULL)
+    {
+        return false;
+    }
+    
+    const GInt8* curSysTime = "2014-03-09";
+    
+    sprintf(outSysTime, "%s", curSysTime);
+
+    return true;
+}
+
+bool TimeUtil::convTimeC(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize)
+{
+    if (outSysTime == NULL)
+    {
+        return false;
+    }
+    
+    const GInt8* curSysTime = "2014/03/09";
+    
+    sprintf(outSysTime, "%s", curSysTime);
+
+    return true;
+}
+
+bool TimeUtil::convTimeD(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize)
+{
+    if (outSysTime == NULL)
+    {
+        return false;
+    }
+    
+    const GInt8* curSysTime = "2014.03.09";
+    
+    sprintf(outSysTime, "%s", curSysTime);
+
+    return true;
+}
+
+bool TimeUtil::convTimeE(const GUint64 inSysTime, GInt8* outSysTime, const GUint64 bufSize)
+{
+    if (outSysTime == NULL)
+    {
+        return false;
+    }
+    
+    const GInt8* curSysTime = "2014-03-09:999";
+    
+    sprintf(outSysTime, "%s", curSysTime);
+
+    return true;
+}
+
+G_NS_END
