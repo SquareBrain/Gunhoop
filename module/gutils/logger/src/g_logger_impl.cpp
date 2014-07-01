7004f1334b7939f9fe41acc6735286408cf2191c
duye <wangwei_cp@163.com>
2014-07-01 22:20:58 +0800

    modify files

--- module/gutils/logger/src/g_logger_impl.cpp
+++ module/gutils/logger/src/g_logger_impl.cpp
@@ -20,57 +20,6 @@
 static const GUint64 DEF_ONE_LINE_BUF_SIZE = 1024;
 
 G_NS_GUTILS_BEG
-  
-GResult LoggerUtil::init()
-{
-    return Logger::GetInstance()->init();
-}
-
-GResult LoggerUtil::uninit()
-{
-    return Logger::GetInstance()->uninit();
-}
-
-void LoggerUtil::printError(const GInt8* module, 
-    const GInt8* file, 
-    const GUint32 line, 
-    const GInt8* function,
-    const GInt8* log)
-{
-    Logger::GetInstance()->printLog(LOG_ERROR, );    
-}
-
-void LoggerUtil::printWarn(const GInt8* module, 
-    const GInt8* file, 
-    const GUint32 line, 
-    const GInt8* function,
-    const GInt8* log)
-{
-}
-
-void LoggerUtil::printInfo(const GInt8* module, 
-    const GInt8* file, 
-    const GUint32 line, 
-    const GInt8* function,
-    const GInt8* log)
-{
-}
-
-void LoggerUtil::printDebug(const GInt8* module, 
-    const GInt8* file, 
-    const GUint32 line, 
-    const GInt8* function,
-    const GInt8* log)
-{
-}
-
-void LoggerUtil::printTrace(const GInt8* module, 
-    const GInt8* file, 
-    const GUint32 line, 
-    const GInt8* function,
-    const GInt8* log)
-{
-}
 
 Logger::Logger()
 {
