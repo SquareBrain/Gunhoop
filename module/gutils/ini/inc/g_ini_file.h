5f3939d19a0cc56f6382f46f0e9fb684531c3356
duye <wangwei_cp@163.com>
2014-07-05 22:48:46 +0800

    add files

---
+++ module/gutils/ini/inc/g_ini_file.h
@@ -0,0 +1,149 @@
+/***********************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+************************************************************************************/
+/**
+* @file		g_ini_file.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2013-11-15
+* @note 
+*
+*  2. 2014-06-24 duye Migration to Gohoop project
+*  1. 2013-11-15 duye Created this file
+* 
+*/
+#pragma once
+
+#include <map>
+#include <string>
+#include <g_type.h>
+#include <g_lock.h>
+
+G_NS_GUTILS_BEG
+
+class IniSection;
+
+/** 
+ * ini file section map
+ * <section_name, ini_section>
+ */
+typedef std::map<std::string, IniSection*> IniSectionMap;
+
+/** 
+ * POSIX condition wrapper
+ */
+class IniFile
+{
+public:
+    IniFile();
+    /**
+     * constructor
+     * @param [in] filePath : file path
+     * @note 
+     */	    
+    IniFile(const std::string& filePath);
+    ~IniFile();
+
+    /**
+     * load file
+     * @param [in] filePath : file path
+     * @return G_YES/G_NO
+     * @note 
+     */	 
+    GResult loadFile(const std::string& filePath);
+
+    /**
+     * import data from buffer
+     * @param [in] data : file data
+     * @return G_YES/G_NO
+     * @note 
+     */	     
+    GResult importData(const std::string& data);
+
+    /**
+     * import data from buffer
+     * @param [in] data : file data
+     * @param [in] length : data length
+     * @return G_YES/G_NO
+     * @note 
+     */	     
+    GResult importData(const GInt8* data, const GUint64 length);    
+
+    /**
+     * get value
+     * @param [in] section : section name
+     * @param [in] paraName : parameter name
+     * @param [out] value : return value
+     * @return G_YES/G_NO
+     * @note 
+     */	 
+    GResult getParaVal(const std::string& section, 
+        const std::string& paraName, 
+        std::string& value);
+
+    /**
+     * set value
+     * @param [in] section : section name
+     * @param [in] paraName : parameter name
+     * @param [in] value : set value
+     * @return G_YES/G_NO
+     * @note 
+     */	 
+    GResult setParaVal(const std::string& section, 
+        const std::string& paraName, 
+        const std::string& value);    
+    /**
+     * del section
+     * @param [in] section : section name
+     * @param [in] paraName : parameter name
+     * @param [in] value : set value
+     * @return G_YES/G_NO
+     * @note 
+     */	 
+    GResult delSection(const std::string& section);
+    
+    /**
+     * del parameter
+     * @param [in] section : section name
+     * @param [in] paraName : parameter name
+     * @param [in] value : set value
+     * @return G_YES/G_NO
+     * @note 
+     */	 
+    GResult delPara(const std::string& section, const std::string& paraName); 
+    
+    /**
+     * save configuration to file
+     * @return G_YES/G_NO
+     * @note 
+     */	 
+    GResult saveFile();  
+
+    /**
+     * save configuration to file
+     * @return G_YES/G_NO
+     * @note 
+     */	 
+    GResult saveFile(const std::string& filePath);    
+
+private:
+    void cleanIniSectionMap();
+    
+    GResult parserSection(const GInt8* data, 
+        const GUint64 length, 
+        GUint64& offset);
+
+    GResult getOneLine(const GInt8* data, 
+        const GUint64 length, 
+        std::string& lineStr);        
+
+private:
+    std::string         m_filePath;
+    IniSectionMap       m_iniSectionMap;
+    GCommon::Mutex      m_mapLock;
+};
+
+G_NS_END
---
+++ module/gutils/ini/src/g_ini_file.cpp
@@ -0,0 +1,328 @@
+/************************************************************************************
+**  
+*    @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+************************************************************************************/
+/**
+* @file		g_ini_file.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2013-11-15
+* @note 
+*
+*  2. 2014-06-26 duye removal to gohoop project
+*  1. 2013-11-15 duye Created this file
+* 
+*/
+#include <g_file.h>
+#include <g_ini_section.h>
+#include <g_ini_file.h>
+
+G_NS_GUTILS_BEG
+
+static const GUint64 INI_TMP_BUF_SIZE = 1024 * 10;
+
+IniFile::IniFile()
+{
+}
+
+IniFile::IniFile(const std::string& filePath)
+{
+    loadFile(m_filePath);
+}
+
+IniFile::~IniFile()
+{
+}
+
+GResult IniFile::loadFile(const std::string& filePath)
+{
+    if (m_filePath == filePath)
+    {
+        return G_YES;
+    }
+
+    if (!m_filePath.empty())
+    {
+        cleanIniSectionMap();    
+    }
+    
+    m_filePath = filePath;
+
+    GCommon::File file(m_filePath.c_str());
+    if (file.openFile(ONLY_READ) != G_YES)
+    {
+        return G_NO;
+    }
+
+    GInt64 fileSize = file.getFileSize();
+    if (fileSize <= 0)
+    {   
+        file.closeFile();
+        return G_NO;
+    }
+    
+    GInt8* buffer = new GInt8[fileSize + 1];
+    GInt64 readSize = file.readFile(buffer, fileSize);
+    file.closeFile();
+    
+    if (readSize != fileSize)
+    {
+        return G_NO;    
+    }
+
+    buffer[readSize] = 0;
+    GResult ret = importData(buffer, fileSize);
+
+    delete [] buffer;
+    buffer = NULL;
+    
+    return ret;
+}
+
+GResult IniFile::importData(const std::string& fileData)
+{
+    return importData(fileData.c_str(), fileData.length());
+}
+
+GResult IniFile::importData(const GInt8* data, const GUint64 length)
+{
+    GUint64 offset = 0;
+    
+    while (offset < length)
+    {
+        switch (data[offset])
+        {
+        case '[':
+            if (parserSection(data, length, offset) != G_YES)
+            {
+                return G_NO;
+            }
+            break;
+        default:
+            offset++;
+            break;
+        }
+    }
+    
+    return G_YES;
+}
+
+GResult IniFile::getParaVal(const std::string& section, 
+    const std::string& paraName, 
+    std::string& value)
+{
+    GCommon::AutoLock autoLock(m_mapLock); 
+    IniSectionMap::iterator iter = m_iniSectionMap.find(section);
+    if (iter == m_iniSectionMap.end())
+    {
+        return G_NO;
+    }
+
+    return iter->second->getPara(paraName, value);
+}
+
+GResult IniFile::setParaVal(const std::string& section, 
+    const std::string& paraName, 
+    const std::string& value)
+{
+    GCommon::AutoLock autoLock(m_mapLock); 
+    IniSectionMap::iterator iter = m_iniSectionMap.find(section);
+    if (iter == m_iniSectionMap.end())
+    {
+        return G_NO;
+    }
+
+    return iter->second->setPara(paraName, value);    
+}
+
+GResult IniFile::delSection(const std::string& section)
+{
+    GCommon::AutoLock autoLock(m_mapLock); 
+    IniSectionMap::iterator iter = m_iniSectionMap.find(section);
+    if (iter == m_iniSectionMap.end())
+    {
+        return G_NO;
+    }
+
+    m_iniSectionMap.erase(iter);
+
+    return G_YES;    
+}
+
+GResult IniFile::delPara(const std::string& section, const std::string& paraName)
+{
+    GCommon::AutoLock autoLock(m_mapLock); 
+    IniSectionMap::iterator iter = m_iniSectionMap.find(section);
+    if (iter == m_iniSectionMap.end())
+    {
+        return G_NO;
+    }
+
+    return iter->second->delPara(paraName);     
+}
+
+GResult IniFile::saveFile()
+{
+    if (m_filePath.empty())
+    {
+        return G_NO;
+    }
+
+    saveFile(m_filePath);
+    
+    return G_YES;
+}
+
+GResult IniFile::saveFile(const std::string& filePath)
+{
+    if (filePath.empty())
+    {
+        return G_NO;
+    }
+
+    GInt8 tmpBuf[INI_TMP_BUF_SIZE] = {0};
+    GUint64 bufPos = 0;
+
+    GCommon::AutoLock autoLock(m_mapLock); 
+    
+    IniSectionMap::iterator iter = m_iniSectionMap.begin();
+    for (; iter != m_iniSectionMap.end(); ++iter)
+    {
+        bufPos += sprintf(tmpBuf + bufPos, "[%s]\n", iter->first.c_str());
+
+        if (bufPos >= INI_TMP_BUF_SIZE)
+        {
+            return G_NO;
+        }       
+        
+        IniSection* section = iter->second;
+        const IniSection::KeyValueMap& keyValueMap = section->getkeyValueMap();
+        IniSection::KeyValueMap::const_iterator iter = keyValueMap.begin();
+        for (; iter != keyValueMap.end(); ++iter)
+        {
+            bufPos += sprintf(tmpBuf + bufPos, "%s=%s\n", iter->first.c_str(), iter->second.c_str()); 
+            if (bufPos >= INI_TMP_BUF_SIZE)
+            {
+                return G_NO;
+            }
+        }
+    }
+
+    tmpBuf[bufPos] = 0;
+
+    GCommon::File file(m_filePath.c_str());
+    if (file.openFile(ONLY_WRITE) != G_YES)
+    {
+        return G_NO;
+    }    
+
+    GUint64 writeBytes = file.writeFile(tmpBuf, bufPos);
+    file.closeFile();
+    
+    if (writeBytes != bufPos)
+    {
+        return G_NO;   
+    }
+    
+    return G_YES;
+}
+
+void IniFile::cleanIniSectionMap()
+{
+    GCommon::AutoLock autoLock(m_mapLock); 
+    
+    IniSectionMap::iterator iter = m_iniSectionMap.begin();
+    while (iter != m_iniSectionMap.end())
+    {
+        delete iter->second;
+        m_iniSectionMap.erase(iter++);   
+    }
+}
+
+GResult IniFile::parserSection(const GInt8* data, 
+    const GUint64 length, 
+    GUint64& offset)
+{
+    GUint64 begPos = offset;
+    GUint64 endPos = offset;
+    bool findSectionEnd = false;
+    
+    while (data[endPos] != 0)
+    {
+        switch (data[endPos++])
+        {
+        case '\r':
+        case '\n':
+        case ';':
+        case '=':
+        case ':':
+            offset = endPos;
+            return G_NO;
+        case ']':
+            findSectionEnd = true;
+            break;
+        default:
+            break;
+        }  
+
+        if (findSectionEnd)
+        {
+            break;
+        }
+    }
+
+    if (endPos - begPos <= 1)
+    {
+        offset = endPos;
+        return G_NO;    
+    }
+
+    std::string sectionName(data + begPos, endPos - begPos);
+    IniSection* section = new IniSection(sectionName);
+
+    std::string lineStr;
+    while (getOneLine(data + endPos, length - offset, lineStr) == G_YES)
+    {
+        offset += lineStr.length();
+        GInt32 midPos = 0;
+        if ((midPos = lineStr.find('=')) == std::string::npos)
+        {
+            continue;       
+        }
+        
+        section->addPara(lineStr.substr(0, midPos), lineStr.substr(midPos + 1));
+    }
+    
+    m_iniSectionMap.insert(std::make_pair(sectionName, section));
+    
+    offset = endPos;
+
+    return G_YES;
+}
+
+GResult IniFile::getOneLine(const GInt8* data, 
+    const GUint64 length, 
+    std::string& lineStr)
+{
+    if (length == 0)
+    {
+        return G_NO;
+    }
+    
+    GUint64 endPos = 0;
+    while (endPos < length)
+    {
+        if (data[endPos++] == '\n')
+        {
+            break;
+        }
+    }
+
+    lineStr.assign(data, endPos);
+
+    return G_YES;
+}
+
+G_NS_END
