db78475e9edae6ea222db968c0318d669dd0b979
duye <wangwei_cp@163.com>
2014-07-10 22:52:36 +0800

    add folder

---
+++ module/gcommon/inc/g_common.h
@@ -0,0 +1,32 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file	    g_type.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2013-11-15
+* @note 
+*
+*  2. 2014-06-21 duye move to gohoop project 
+*  1. 2013-11-15 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <g_system.h>
+
+/** 
+ * name space define
+ */
+#define G_NS_END	        }
+#define G_NS_GBASE_BEG	    namespace GBase {
+#define G_NS_GCOMMON_BEG	namespace GCommon {
+#define G_NS_GDATA_BEG	    namespace GData {
+#define G_NS_GDFS_BEG	    namespace GDfs {
+#define G_NS_GMAPREDUCE_BEG	namespace GMapreduce {
+#define G_NS_GUTILS_BEG	    namespace GUtils {
