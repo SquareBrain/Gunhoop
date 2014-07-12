8c60d0b8da398695cbe98a6d5f2540f994ec3855
duye <wangwei_cp@163.com>
2014-07-12 21:15:21 +0800

    add files

---
+++ module/gutils/gxml/inc/g_xml.h
@@ -0,0 +1,28 @@
+/************************************************************************************
+**  
+*    @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+************************************************************************************/
+/**
+* @file		g_xml.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2013-11-15
+* @note 
+*
+*  2. 2014-06-24 duye Migration to Gohoop project
+*  1. 2013-11-15 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <g_system.h>
+
+class GXmlFile
+{
+public:
+    GXmlFile();
+    ~GXmlFile();
+};
