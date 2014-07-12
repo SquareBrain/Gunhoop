dc5d39217b2b77fddeabf67292e78b1e31c73280
duye <wangwei_cp@163.com>
2014-07-12 20:54:48 +0800

    add files

---
+++ module/gutils/xml/src/g_xml.cpp
@@ -0,0 +1,27 @@
+/************************************************************************************
+**  
+*    @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+************************************************************************************/
+/**
+* @file		g_xml_parser.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2013-11-15
+* @note 
+*
+*  2. 2014-06-24 duye Migration to Gohoop project
+*
+*  1. 2013-11-15 duye Created this file
+* 
+*/ 
+#include <g_xml_parser.h>
+
+XmlParser::XmlParser()
+{
+}
+
+XmlParser::~XmlParser()
+{
+}
