dea07f80dd8dfc43943105129438f6558be46451
duye <wangwei_cp@163.com>
2014-07-17 23:08:19 +0800

    modify files

--- module/gutils/gxml/inc/g_xml.h
+++ module/gutils/gxml/inc/g_xml.h
@@ -19,6 +19,7 @@
 #pragma once
 
 #include <g_system.h>
+#include <string>
 
 const int NPT_ERROR_XML_INVALID_NESTING = NPT_ERROR_BASE_XML - 0;
 const int NPT_ERROR_XML_TAG_MISMATCH    = NPT_ERROR_BASE_XML - 1;
@@ -28,144 +29,58 @@ const int NPT_ERROR_XML_MULTIPLE_ROOTS  = NPT_ERROR_BASE_XML - 3;
 #define NPT_XML_ANY_NAMESPACE "*"
 #define NPT_XML_NO_NAMESPACE  NULL
 
-/*----------------------------------------------------------------------
-|   forward declarations
-+---------------------------------------------------------------------*/
 class NPT_XmlProcessor;
 
-/*----------------------------------------------------------------------
-|   NPT_XmlAttribute
-+---------------------------------------------------------------------*/
-class NPT_XmlAttribute
+class GXmlAttribute
 {
  public:
-    // methods
-    NPT_XmlAttribute(const char* name, const char* value);
-    NPT_XmlAttribute(const char* prefix, const char* name, const char* value) :
-        m_Prefix(prefix), m_Name(name), m_Value(value) {}
-    const NPT_String& GetPrefix() const { return m_Prefix; }
-    const NPT_String& GetName()   const { return m_Name;   }
-    const NPT_String& GetValue()  const { return m_Value;  }
-    void              SetValue(const char* value) { m_Value = value; }
-
+    GXmlAttribute(const std::string& name, const std::string& value);
+    ~GXmlAttribute();
+    const std::string& getName();
+    void setValue(const std::string& value);
+    const std::string& getValue();
+    
  private:
-    // members
-    NPT_String m_Prefix;
-    NPT_String m_Name;
-    NPT_String m_Value;
-
-    NPT_XmlAttribute(const NPT_XmlAttribute& attribute) :
-        m_Prefix(attribute.m_Prefix), 
-        m_Name(attribute.m_Name), 
-        m_Value(attribute.m_Value) {}
-    NPT_XmlAttribute& operator=(const NPT_XmlAttribute& a);
-
-    // friends
-    friend class NPT_XmlAttributeFinder;
-    friend class NPT_XmlAttributeFinderWithPrefix;
+    std::string     m_name;
+    std::string     m_value;
 };
 
-/*----------------------------------------------------------------------
-|   NPT_XmlNamespaceMap
-+---------------------------------------------------------------------*/
-class NPT_XmlNamespaceMap
-{
-public:
-    // destructor
-    ~NPT_XmlNamespaceMap();
-
-    // methods   
-    NPT_Result        SetNamespaceUri(const char* prefix, const char* uri);
-    const NPT_String* GetNamespaceUri(const char* prefix);
-    const NPT_String* GetNamespacePrefix(const char* uri);
-
-private:
-    // types
-    class Entry {
-    public:
-        // constructor
-        Entry(const char* prefix, const char* uri) : 
-            m_Prefix(prefix), m_Uri(uri) {}
-
-        // members
-        NPT_String m_Prefix;
-        NPT_String m_Uri;
-    };
-
-    // members
-    NPT_List<Entry*> m_Entries;
-
-    // friends
-    friend class NPT_XmlWriter;
-    friend class NPT_XmlNodeWriter;
-    friend class NPT_XmlNodeCanonicalWriter;
-};
-
-/*----------------------------------------------------------------------
-|   NPT_XmlNode
-+---------------------------------------------------------------------*/
-class NPT_XmlElementNode;
-class NPT_XmlTextNode;
-class NPT_XmlNode
+class GXmlNode
 {
  public:
-    // types
     typedef enum {
         DOCUMENT,
         ELEMENT,
         TEXT
-    } Type;
+    } GNodeType;
 
-    // methods
-    NPT_XmlNode(Type type) : m_Type(type), m_Parent(NULL) {}
-    virtual     ~NPT_XmlNode() {}
-    Type         GetType() const { return m_Type; }
-    NPT_XmlNode* GetParent() const { return m_Parent; }
-
-    // type casting
-    virtual NPT_XmlElementNode*       AsElementNode()       { return NULL; }
-    virtual const NPT_XmlElementNode* AsElementNode() const { return NULL; }
-    virtual NPT_XmlTextNode*          AsTextNode()          { return NULL; }
-    virtual const NPT_XmlTextNode*    AsTextNode() const    { return NULL; }
-
- protected:
-    // methods
-    virtual void SetParent(NPT_XmlNode* parent) { m_Parent = parent; }
-
-    // members  
-    Type         m_Type;
-    NPT_XmlNode* m_Parent;
-
-    // friends
-    friend class NPT_XmlNodeFinder;
-    friend class NPT_XmlSerializer;
-    friend class NPT_XmlWriter;
-    friend class NPT_XmlElementNode; // to allow access to SetParent()
+    explicit GXmlNode(GNodeType type);
+    virtual ~GXmlNode();
+    
+    GNodeType getType();
+    GXmlNode* getParent();
+    void setParent(GXmlNode* parent);
+    
+private:
+    GNodeType       m_type;
+    GXmlNode*       m_parent;
 };
 
-/*----------------------------------------------------------------------
-|   NPT_XmlElementNode
-+---------------------------------------------------------------------*/
-class NPT_XmlElementNode : public NPT_XmlNode
+typedef std::list<GXmlNode*> GXmlNodeList;
+
+class GXmlElementNode : public GXmlNode
 {
- public:
-    // methods
-                            NPT_XmlElementNode(const char* tag);
-                            NPT_XmlElementNode(const char* prefix, const char* tag);
-    virtual                ~NPT_XmlElementNode();
-    NPT_List<NPT_XmlNode*>& GetChildren() { return m_Children; }
-    const NPT_List<NPT_XmlNode*>& 
-                            GetChildren() const { return m_Children; }
-    NPT_XmlElementNode*     GetChild(const char* tag, 
-                                     const char* namespc = NPT_XML_NO_NAMESPACE,
-                                     NPT_Ordinal n=0) const;
-    NPT_Result              AddChild(NPT_XmlNode* child);
-    NPT_Result              SetAttribute(const char* prefix,
-                                         const char* name, 
-                                         const char* value);
-    NPT_Result              SetAttribute(const char* name, 
-                                         const char* value);
-    NPT_Result              AddText(const char* text); 
+public:
+    GXmlElementNode(const std::string& tag);
+    virtual GXmlElementNode();
+    
+    GXmlNodeList& getChildren();
+    const GXmlNodeList& getChildren() const;
+    GXmlElementNode* getChild(std::string& tag) const;
+    
+    GResult addChild(GXmlNode* child);
+    GResult setAttribute(const std::string& name, const std::string& value);
+    GResult AddText(const std::string& text); 
     NPT_List<NPT_XmlAttribute*>& 
                             GetAttributes() { return m_Attributes; }
     const NPT_List<NPT_XmlAttribute*>&
@@ -218,93 +133,56 @@ protected:
     friend class NPT_XmlNamespaceCollapser;
 };
 
-/*----------------------------------------------------------------------
-|   NPT_XmlTextNode
-+---------------------------------------------------------------------*/
-class NPT_XmlTextNode : public NPT_XmlNode
+class GXmlTextNode : public NPT_XmlNode
 {
  public:
-    // types
     typedef enum {
         CHARACTER_DATA,
         IGNORABLE_WHITESPACE,
         CDATA_SECTION,
         ENTITY_REFERENCE,
         COMMENT
-    } TokenType;
+    } GTokenType;
 
-    // constructor
-    NPT_XmlTextNode(TokenType token_type, const char* text);
+    GXmlTextNode(GTokenType tokenType, const std::string& text);
 
-    // methods
-    const NPT_String& GetString() const { return m_Text; }
-
-    // type casting
-    NPT_XmlTextNode*       AsTextNode()       { return this; }
-    const NPT_XmlTextNode* AsTextNode() const { return this; }
+    const std::string& getString();
 
  private:
-    // members  
-    TokenType  m_TokenType;
-    NPT_String m_Text;
+    GTokenType        m_TokenType;
+    std::string&      m_Text;
 };
 
-/*----------------------------------------------------------------------
-|   NPT_XmlParser
-+---------------------------------------------------------------------*/
-class NPT_XmlParser
+class GXmlDoc
 {
  public:
-    // methods
-             NPT_XmlParser(bool keep_whitespace = true);
-    virtual ~NPT_XmlParser();
-    virtual  NPT_Result Parse(const char*   xml, 
-                              NPT_XmlNode*& tree,
-                              bool          incremental=false);
-    virtual  NPT_Result Parse(const char*   xml, 
-                              NPT_Size      size,
-                              NPT_XmlNode*& tree,
-                              bool          incremental=false);
-    virtual  NPT_Result Parse(NPT_InputStream& stream, 
-                              NPT_XmlNode*&    tree,
-                              bool             incremental=false);
-    virtual  NPT_Result Parse(NPT_InputStream& stream, 
-                              NPT_Size&        size,
-                              NPT_XmlNode*&    tree,
-                              bool             incremental=false);
+    GXmlDoc();
+    explicit GXmlDoc(const GInt8* xmlDocPath);
+    ~GXmlDoc();
     
- protected:
-    // NPT_XmlHandler methods
+    GResult Load(const GInt8* xmlDocPath);
+    
     NPT_Result OnStartElement(const char* name);
     NPT_Result OnElementAttribute(const char* name, const char* value);
     NPT_Result OnEndElement(const char* name);
     NPT_Result OnCharacterData(const char* data, unsigned long size);
-    void       RemoveIgnorableWhitespace();
-
-    // members
-    NPT_XmlProcessor*   m_Processor;
-    NPT_XmlElementNode* m_Root;
-    NPT_XmlElementNode* m_CurrentElement;
-    bool                m_KeepWhitespace;
+    void RemoveIgnorableWhitespace();
 
 private:
-    void Reset();
-    
-    // friends
-    friend class NPT_XmlProcessor;
+    GXmlProcessor*      m_Processor;
+    GXmlElementNode*    m_Root;
+    GXmlElementNode*    m_CurrentElement;
+    bool                m_KeepWhitespace;
 };
 
-/*----------------------------------------------------------------------
-|   NPT_XmlSerializer
-+---------------------------------------------------------------------*/
-class NPT_XmlSerializer
+class GXmlSerializer
 {
 public:
-    // methods
-                       NPT_XmlSerializer(NPT_OutputStream* output,
-                                         NPT_Cardinal      indentation = 0,
-                                         bool              shrink_empty_elements = true,
-                                         bool              add_xml_decl = false);
+   NPT_XmlSerializer(NPT_OutputStream* output,
+         NPT_Cardinal      indentation = 0,
+         bool              shrink_empty_elements = true,
+         bool              add_xml_decl = false);
+         
     virtual           ~NPT_XmlSerializer();
     virtual NPT_Result StartDocument();
     virtual NPT_Result EndDocument();
