c6cb58547db20cbc8ece916b11c6d0b6c294074c
duye <wangwei_cp@163.com>
2014-08-22 08:14:22 +0800

    add files

---
+++ module/gtask/gtaskmaster/src/g_master_main.cpp
@@ -0,0 +1,27 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file     g_master_main.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-06-22
+* @note 
+*
+*  1. 2014-06-22 duye Created this file
+* 
+*/
+#include <stdio.h>
+#include <g_system.h>
+#include <g_master.h>
+
+int main()
+{
+	printf("Gohoop Master Start.\n");
+	GMapreduce::Master master;
+	master.start();
+	return 0;
+}
