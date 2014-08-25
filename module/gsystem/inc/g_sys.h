a7a0f9fb7fb6d01502402eeff3b00f0531ba7f34
duye <wangwei_cp@163.com>
2014-08-25 16:18:51 +0800

    Update g_sys.h

--- module/gsystem/inc/g_sys.h
+++ module/gsystem/inc/g_sys.h
@@ -22,52 +22,55 @@
 
 namespace gsys {
 
-typedef enum
+class GSystem
 {
-	G_SHELL_R = 1,
-	G_SHELL_W
-} GShellMode;
-
-/**
- * @brief sleep
- * @param [in] time : second
- * @note 
- */		
-static void sleep(const GUint64 time);
-
-/**
- * @brief usleep
- * @param [in] time : millisecond
- * @note 
- */		
-static void usleep(const GUint64 time);
-
-/**
- * @brief usleep
- * @param [out] buffer : out buffer
- * @param [in] size : out buffer size
- * @param [in] args : parameters
- * @return string size
- * @note 
- */		
-static GUint64 format(GInt8* buffer, const GUint64 size, const GInt8* args, ...);   
-
-/**
- * @brief execute shell commond
- * @param [in] cmd : commond line
- * @param [in] mode : read/write for pipe
- * @param [out] buffer : output buffer
- * @param [in] size : output buffer size
- * @return G_YES/G_NO
- * @note 
- */		
-static GResult shell(const GInt8* cmd, const GShellMode mode, GInt8* buffer, const GUint32 size);   
-
-/**
- * @brief get system time
- * @return time(microsecond)
- * @note 
- */		
-static GUint64 getSysTime();   
+	/**
+	 * @brief sleep
+	 * @param [in] time : second
+	 * @note 
+	 */		
+	static void sleep(const GUint64 time);
+	
+	/**
+	 * @brief usleep
+	 * @param [in] time : millisecond
+	 * @note 
+	 */		
+	static void usleep(const GUint64 time);
+	
+	/**
+	 * @brief usleep
+	 * @param [out] buffer : out buffer
+	 * @param [in] size : out buffer size
+	 * @param [in] args : parameters
+	 * @return string size
+	 * @note 
+	 */		
+	static GUint64 format(GInt8* buffer, const GUint64 size, const GInt8* args, ...);   
+	
+	/**
+	 * @brief execute shell commond
+	 * @param [in] cmd : commond line
+	 * @return G_YES/G_NO
+	 * @note 
+	 */		
+	static GResult shell(const GInt8* cmd);  
+	
+	/**
+	 * @brief execute shell commond
+	 * @param [in] cmd : commond line
+	 * @param [out] buffer : output buffer
+	 * @param [in] size : output buffer size
+	 * @return G_YES/G_NO
+	 * @note 
+	 */		
+	static GResult shell(const GInt8* cmd, GInt8* buffer, const GUint32 size);   
+	
+	/**
+	 * @brief get system time
+	 * @return time(microsecond)
+	 * @note 
+	 */		
+	static GUint64 getSysTime();   
 };
 }
