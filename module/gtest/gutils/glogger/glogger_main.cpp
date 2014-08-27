7dace6b4cd49b43f8fbfe0a84915c983f710f4c6
duye <wangwei_cp@163.com>
2014-08-27 14:08:46 +0800

    Update glogger_main.cpp

--- module/gtest/gutils/glogger/glogger_main.cpp
+++ module/gtest/gutils/glogger/glogger_main.cpp
@@ -2,11 +2,19 @@
 
 int main()
 {
-    // G_LOG_INIT();
+    G_LOG_INIT();
 
-    //G_LOG_ERROR("gutils.logger.example", "error print test");
+    G_LOG_ERROR("gutils.logger.example", "error print test");
+    G_LOG_WARN("gutils.logger.example", "error print test");
+    G_LOG_INFO("gutils.logger.example", "error print test");
+    G_LOG_DEBUG("gutils.logger.example", "error print test");
+    G_LOG_TRACE("gutils.logger.example", "error print test");
     
-    //G_LOG_UNINIT();
+    G_LOG_ERROR("gutils.logger.example1", "error print test");
+    G_LOG_WARN("gutils.logger.example1", "error print test");
+    G_LOG_INFO("gutils.logger.example1", "error print test");
+    G_LOG_DEBUG("gutils.logger.example1", "error print test");
+    G_LOG_TRACE("gutils.logger.example1", "error print test");    
     
     return 0;
 }
