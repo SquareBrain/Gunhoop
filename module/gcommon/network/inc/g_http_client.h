b38f9e12cec32f3f90e45d6fa69f73fb8190ecfb
duye <wangwei_cp@163.com>
2014-08-29 10:50:26 +0800

    Create g_http_client.h

---
+++ module/gcommon/network/inc/g_http_client.h
@@ -0,0 +1,30 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_http_client.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-08-29
+* @note 
+*
+*  1. 2014-08-29 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <g_system.h>
+
+namespace gcom {
+
+class HttpClient
+{
+public:
+	HttpClient() {}
+	~HttpClient() {}
+};
+}
