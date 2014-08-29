c4beea5355f8f1ff99a997a2e8f5c2d0a640f559
duye <wangwei_cp@163.com>
2014-08-29 14:37:35 +0800

    Update g_network.cpp

--- module/gcommon/network/src/g_network.cpp
+++ module/gcommon/network/src/g_network.cpp
@@ -19,4 +19,146 @@
 #include <g_network.h>
 
 namespace gcom {
+    
+NetWorkConv::NetWorkConv() {}
+NetWorkConv::~NetWorkConv() {}
+
+GResult NetWorkConv::ipToInt(const std::string& strIP, GUint32& intIP)
+{
+    std::string tmpStr;
+    GUint16 begPos = 0;
+    
+    for (GUint16 i = 0; i < strIP.size(); i++)
+    {
+        if (strIP[i] != '.' && (strIP[i] < '0' || strIP[i] > '9'))
+        {
+            return G_NO;
+        }
+        
+        if (i == 0 && strIP[i] == '.')
+        {
+            return G_NO;
+        }
+        
+        if (strIP[i] == '.')
+        {
+            tmpStr.assign(strIP.subString(beg, i - beg));
+        }
+    }
+    
+    return G_YES;
+}
+
+GResult NetWorkConv::ipToString(const GUint32 intIP, std::string& strIP)
+{
+    return G_YES;
+}
+
+GResult NetWorkConv::macToInteger(const std::string& strMac, GUint32& intMac)
+{
+    return G_YES;
+}
+
+GResult NetWorkConv::macToString(const GUint32 intMac, std::string& strMac)
+{
+    return G_NO;
+}
+
+IPAddr::IPAddr(const GUint32 ip) : m_ip(ip) 
+{
+    NetWorkConv::ipToString(m_ip, m_ipStr);
+}
+
+IPAddr::IPAddr(const GInt8* ip) : m_ipStr(ip)
+{
+    NetWorkConv::ipToInteger(m_ipStr, m_ip);
+}
+
+IPAddr::IPAddr(const std::string& ip) : m_ipStr(ip)
+{
+    NetWorkConv::ipToInteger(m_ipStr, m_ip);
+}
+
+IPAddr::~IPAddr() 
+{
+}
+
+GUint32 IPAddr::getIP() const
+{
+    return m_ip;
+}
+
+const std::string& IPAddr::getIP(const GUint32 ip) const 
+{
+    return m_ipStr;
+}
+
+IPv6Addr::IPv6Addr(const GInt8* ip) : m_ipStr(ip)
+{
+}
+
+IPv6Addr::IPv6Addr(const std::string& ip) : m_ipStr(ip)
+{
+}
+
+const std::string& IPv6Addr::getIP() const
+{
+    return m_ipStr;
+}
+
+MacAddr::MacAddr(const GUint64 mac) : m_mac(mac)
+{
+    NetWorkConv::macToString(m_mac, m_macStr);
+}
+
+MacAddr::MacAddr(const GInt8* mac) : m_macStr(mac)
+{
+    NetWorkConv::macToInteger(m_macStr, m_mac);
+}
+
+MacAddr::MacAddr(const std::string& mac) : m_macStr(mac)
+{
+    NetWorkConv::macToInteger(m_macStr, m_mac);
+}
+
+IPPortPair::IPPortPair(const IPAddr& ipAddr, const GUint16 port) : m_ipv4(ipAddr), m_port(port)
+{
+}
+
+IPPortPair::IPPortPair(const IPv6Addr& ipAddr, const GUint16 port) : m_ipv6(ipAddr), m_port(port)
+{
+}
+
+void IPPortPair::setIPv4(const IPAddr& ipAddr)
+{
+    m_ipv4 = ipAddr;
+}
+
+const IPAddr& IPPortPair::getIPv4() const
+{
+    return m_ipv4;
+}
+
+void IPPortPair::setIPv6(const IPv6Addr& ipAddr)
+{
+    m_ipv6 = ipAddr;
+}
+
+const IPv6Addr& IPPortPair::getIPv6() const
+{
+    return m_ipv6;
+}
+
+void IPPortPair::setPort(const GUint16 port)
+{
+    m_port = port;
+}
+
+GUint16 IPPortPair::getPort() const
+{
+    return m_port;
+}
+
+
+
 }
