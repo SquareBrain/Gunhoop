56e160eb7a05c4a0f92d87db4518c9a5f35c2b16
duye <wangwei_cp@163.com>
2014-09-01 15:06:47 +0800

    Update g_socket.h

--- module/gsystem/inc/g_socket.h
+++ module/gsystem/inc/g_socket.h
@@ -83,6 +83,9 @@ private:
 	socklen_t		m_sockLen;	
 };
 
+/** 
+ * @brief server socket class
+ */
 class SockTransfer
 {
 public:
@@ -117,24 +120,16 @@ public:
 		const GUint64 size, 
 		const GInt32 flags);	
 };
-	
+
 /** 
  * @brief server socket class
  */
-class ServerSocket
+class Socket
 {
 public:
-	ServerSocket();
-	
-	/**
-	 * @brief constructor
-	 * @param [in] ip : ip address
-	 * @param [in] port : port
-	 * @note 
-	 */			
-	 explicit ServerSocket(const GUint32 ip, const GUint16 port);
-	 
-	~ServerSocket();
+	Socket();
+	Socket(const GUint32 ip, const GUint16 port);
+	~Socket();
 	
 	/**
 	 * @brief set/get IP address
@@ -155,7 +150,7 @@ public:
 	 * @return G_YES/G_NO
 	 * @note 
 	 */		
-	GResult init(const GInt32 domain = AF_INET, const GInt32 type = SOCK_STREAM);
+	virtual GResult init(const GInt32 domain = AF_INET, const GInt32 type = SOCK_STREAM);
 	
 	/**
 	 * @brief shutdown connecting
@@ -163,28 +158,7 @@ public:
 	 * @return G_YES/G_NO
 	 * @note 
 	 */	
-	GResult uninit(const GInt32 how = 0);	
-	
-	/**
-	 * @brief bind address and port
-	 * @return G_YES/G_NO
-	 * @note 
-	 */	
-	GResult bind();	
-	
-	/**
-	 * @brief listen port
-	 * @return G_YES/G_NO
-	 * @note 
-	 */	
-	GResult listen();	
-	
-	/**
-	 * @brief waitting for connect
-	 * @return G_YES/G_NO
-	 * @note 
-	 */	
-	GResult accept();	
+	virtual GResult uninit(const GInt32 how = 0);	
 	
 	/**
 	 * @brief send data
@@ -206,15 +180,54 @@ public:
 	 */	
 	GInt64 recv(GUint8* buffer, const GUint64 size, const GInt32 flags = 0);	
 	
+protected:
+	SockEntity	m_sockEntity;
+	
 private:
 	/**
 	 * @brief setting socket options 
 	 * @return G_YES/G_NO
 	 */		
 	GResult initOption();
+};
 	
-private:
-	SockEntity	m_sockEntity;
+/** 
+ * @brief server socket class
+ */
+class ServerSocket
+{
+public:
+	ServerSocket();
+	
+	/**
+	 * @brief constructor
+	 * @param [in] ip : ip address
+	 * @param [in] port : port
+	 * @note 
+	 */			
+	 explicit ServerSocket(const GUint32 ip, const GUint16 port);
+	~ServerSocket();
+	
+	/**
+	 * @brief bind address and port
+	 * @return G_YES/G_NO
+	 * @note 
+	 */	
+	GResult bind();	
+	
+	/**
+	 * @brief listen port
+	 * @return G_YES/G_NO
+	 * @note 
+	 */	
+	GResult listen();	
+	
+	/**
+	 * @brief waitting for connect
+	 * @return G_YES/G_NO
+	 * @note 
+	 */	
+	GResult accept();	
 };
 
 /** 
@@ -235,71 +248,10 @@ public:
 	~ClientSocket();
 	
 	/**
-	 * @brief set/get IP address
-	 * @return
-	 */		
-	void setIP(const GUint32 ip);
-	GUint32 getIP() const;
-
-	/**
-	 * @brief set/get port
-	 * @return 
-	 */		
-	void setPort(const GUint32 port) const;
-	GUint32 getPort() const;
-
-	/**
-	 * @brief init socket
-	 * @param [in] domain : domain
-	 * @param [in] type : socket type
-	 * @return G_YES/G_NO
-	 * @note 
-	 */		
-	GResult init(const GInt32 domain = AF_INET, const GInt32 type = SOCK_STREAM);
-	
-	/**
-	 * @brief shutdown connecting
-	 * @param [in] how : way
-	 * @return G_YES/G_NO
-	 * @note 
-	 */	
-	GResult uninit(const GInt32 how = 0);	
-	
-	/**
 	 * @brief connect socket
 	 * @return G_YES/G_NO
 	 * @note 
 	 */		
-	GResult connect();	
-
-	/**
-	 * @brief send data
-	 * @param [in] data : send data
-	 * @param [in] len : data length
-	 * @param [in] flags : flags
-	 * @return size/-1
-	 * @note 
-	 */		
-	GInt64 send(const GUint8* data, const GUint64 len, const GInt32 flags = MSG_NOSIGNAL);
-	
-	/**
-	 * @brief receive data
-	 * @param [out] buffer : output buffer
-	 * @param [in] bufferSize : buffer size
-	 * @param [in] flags : flags
-	 * @return size/-1
-	 * @note 
-	 */	
-	GInt64 recv(GUint8* buffer, const GUint64 size, const GInt32 flags = 0);
-
-private:
-	/**
-	 * @brief setting socket options 
-	 * @return G_YES/G_NO
-	 */		
-	GResult initOption();
-
-private:
-	SocketEntity	m_socketEntity;				
+	GResult connect();
 };
 }
