874caad23cc5f58be951cd575eaa7faf2ebf73f4
duye <wangwei2135@gmail.com>
2014-09-25 11:35:41 +0800

    Update g_epoll.h

--- module/gsystem/inc/g_epoll.h
+++ module/gsystem/inc/g_epoll.h
@@ -16,3 +16,50 @@
 */
 
 #pragma once
+
+#include <g_type.h>
+#include <g_socket.h>
+
+namespace gsys {
+  
+class EpollEvent
+{
+public:
+    EpollEvent();
+    ~EpollEvent();
+};
+
+/**
+ * @brief epoll server user interface
+ */
+
+class EpollServerInterface
+{
+public:
+    virtual EpollServerInterface();
+    virtual GResult onAccept(const EpollEvnet& epollEvent) = 0;
+};
+
+/**
+ * @brief epoll server wrapper
+ */
+class EpollServer : public ThreadTask
+{
+public:
+    explicit EpollServer(EpollServerInterface* interface = nullptr);
+    explicit EpollServer(const GUint32 server_ip, const GUint16 port, EpollServerInterface* interface = nullptr);
+    ~EpollServer();
+    
+    GResult init();
+    
+    void setServerIP(const GUint32 server_ip);
+    void setPort(const GUint16 port);
+    
+private:
+    GResult run();
+    
+private:
+    Socket      m_socket;
+};
+
+}
