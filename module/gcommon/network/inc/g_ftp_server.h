3089303c6931eaa89e854e7982ea6443fdbe70d3
duye <wangwei2135@gmail.com>
2014-09-28 16:14:09 +0800

    Create g_ftp_server.h

---
+++ module/gcommon/network/inc/g_ftp_server.h
@@ -0,0 +1,18 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_ftp_server.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-09-24
+* @note 
+*
+*  1. 2014-09-24 duye Created this file
+* 
+*/
+
+#pragma once
