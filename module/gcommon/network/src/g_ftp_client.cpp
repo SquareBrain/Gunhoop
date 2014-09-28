1583cc0657f64da20bc409df55bc625e394164ac
duye <wangwei2135@gmail.com>
2014-09-28 17:28:53 +0800

    Create g_ftp_client.cpp

---
+++ module/gcommon/network/src/g_ftp_client.cpp
@@ -0,0 +1,16 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_ftp_client.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-09-28
+* @note 
+*
+*  1. 2014-09-28 duye Created this file
+* 
+*/
