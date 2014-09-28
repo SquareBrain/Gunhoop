509c79a771735796f1de1f54545d2107cebd9760
duye <wangwei2135@gmail.com>
2014-09-28 15:03:39 +0800

    Create g_cfg_mgr.h

---
+++ module/gcommon/function/inc/g_cfg_mgr.h
@@ -0,0 +1,64 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file     g_cfg_mgr.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-09-28
+* @note 
+*
+*  1. 2014-09-28 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <string>
+
+#include <g_type.h>
+#include <g_system.h>
+#include <g_utils.h>
+
+namespace gcom {
+
+/**
+ * @brief configuration manager class
+ */
+class CfgMgr
+{
+public:
+    CfgMgr();
+    explicit CfgMgr(const std::string& cfg_file_path);
+    ~CfgMgr();
+    
+    /**
+     * @brief set configuration value
+     * @param [in] path : parameter path, like : dfscfg.server.port
+     * @param [in] value : set value
+     * @return G_YES/G_NO
+     */
+    GResult setValue(const std::string& path, const bool value);
+    GResult setValue(const std::string& path, const GInt32 value);
+    GResult setValue(const std::string& path, const GInt64 value);
+    GResult setValue(const std::string& path, const GUInt32 value);
+    GResult setValue(const std::string& path, const GUInt64 value);
+    GResult setValue(const std::string& path, const GInt8* value);
+    GResult setValue(const std::string& path, const std::string& value);
+    
+    /**
+     * @brief get configuration value
+     * @param [in] path : parameter path, like : dfscfg.server.port
+     * @param [out] value : get value
+     * @return G_YES/G_NO
+     */
+    GResult getValue(const std::string& path, bool value);
+    
+private:
+    std::string     m_cfgFilePath;
+    XmlDocument     m_cfgDoc;
+};
+}
