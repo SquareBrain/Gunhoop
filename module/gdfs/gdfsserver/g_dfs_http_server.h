cff236232ed76340989ecdf18dfdc8fc6ec793f0
duye <wangwei2135@gmail.com>
2014-09-28 15:57:22 +0800

    Create g_dfs_http_server.h

---
+++ module/gdfs/gdfsserver/g_dfs_http_server.h
@@ -0,0 +1,22 @@
+/************************************************************************************
+**  
+* @copyright (c) 2010-2019, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file	    g_dfs_http_server.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-09-28
+* @note 
+*
+*  1. 2014-09-28 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <g_common.h>
+
+/**
