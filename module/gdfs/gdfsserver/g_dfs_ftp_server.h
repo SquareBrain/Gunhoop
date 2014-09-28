be969eafaac97c8be9e837c9fa2024230b9e9bec
duye <wangwei2135@gmail.com>
2014-09-28 15:58:41 +0800

    Create g_dfs_ftp_server.h

---
+++ module/gdfs/gdfsserver/g_dfs_ftp_server.h
@@ -0,0 +1,20 @@
+/************************************************************************************
+**  
+* @copyright (c) 2010-2019, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file	    g_dfs_ftp_server.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-09-28
+* @note 
+*
+*  1. 2014-09-28 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <g_common.h>
