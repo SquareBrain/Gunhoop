b15b042c1f21f3bcbdabb89c16da6f05c5f9a4f6
duye <wangwei2135@gmail.com>
2014-09-28 17:00:40 +0800

    Create g_rpc_client.h

---
+++ module/gcommon/network/inc/g_rpc_client.h
@@ -0,0 +1,31 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_rpc_client.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-08-29
+* @note 
+*
+*  1. 2014-08-29 duye Created this file
+* 
+*/
+
+#pragma once
+
+#include <g_system.h>
+
+namespace gcom {
+
+class RpcClient
+{
+public:
+	RpcClient() {}
+	~RpcClient() {}
+};
+
+}
