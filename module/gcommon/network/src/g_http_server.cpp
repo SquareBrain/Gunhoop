13f4e186dc5a83e471ed7a2c237b6cd945ea3ce3
duye <wangwei2135@gmail.com>
2014-09-29 09:31:45 +0800

    Create g_http_server.cpp

---
+++ module/gcommon/network/src/g_http_server.cpp
@@ -0,0 +1,16 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_http_server.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-08-30
+* @note 
+*
+*  1. 2014-08-30 duye Created this file
+* 
+*/
