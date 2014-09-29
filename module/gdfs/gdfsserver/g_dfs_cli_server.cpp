ab0e5b4f7430dffb618c35032cd65b2bde976ee9
duye <wangwei2135@gmail.com>
2014-09-29 09:39:29 +0800

    Create g_dfs_cli_server.cpp

---
+++ module/gdfs/gdfsserver/g_dfs_cli_server.cpp
@@ -0,0 +1,16 @@
+/************************************************************************************
+**  
+* @copyright (c) 2010-2019, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file	    g_dfs_cli_server.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-09-30
+* @note 
+*
+*  1. 2014-09-30 duye Created this file
+* 
+*/
