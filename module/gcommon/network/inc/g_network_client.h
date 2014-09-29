36c009ef8889e9a2928deb45906329895cc9bce9
duye <wangwei2135@gmail.com>
2014-09-29 09:52:35 +0800

    Create g_network_client.h

---
+++ module/gcommon/network/inc/g_network_client.h
@@ -0,0 +1,18 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_network_client.h
+* @version     
+* @brief      
+* @author	duye
+* @date		2014-09-30
+* @note 
+*
+*  1. 2014-09-30 duye Created this file
+* 
+*/
+
+/**
