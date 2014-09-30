c5f86b2bef8dc0582cdcb958fbe43f391990034f
duye <wangwei2135@gmail.com>
2014-09-30 10:49:39 +0800

    Create g_network_client.cpp

---
+++ module/gcommon/network/src/g_network_client.cpp
@@ -0,0 +1,21 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_network_client.cpp
+* @version     
+* @brief      
+* @author	duye
+* @date		2014-09-30
+* @note 
+*
+*  1. 2014-09-30 duye Created this file
+* 
+*/
+#include <g_network_client.h>
+
+namespace gcom {
+
+}
