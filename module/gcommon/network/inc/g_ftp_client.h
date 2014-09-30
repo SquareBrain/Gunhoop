21d5a35cf6f9f717e02f98fb1f49cb298c11113e
duye <wangwei2135@gmail.com>
2014-09-30 14:17:58 +0800

    Update g_ftp_client.h

--- module/gcommon/network/inc/g_ftp_client.h
+++ module/gcommon/network/inc/g_ftp_client.h
@@ -34,35 +34,35 @@ public:
 	explicit FtpClient(const IPPortPair& server_addr, const GInt8* interface = "eth0");
 	virtual ~FtpClient();
 	
-    /**
-     * @brief to connect ftp server
-     * @return G_YES/G_NO
-     * @note derive class implemention
-     */
-    GResult connect();
+	/**
+	 * @brief to connect ftp server
+	 * @return G_YES/G_NO
+	 * @note derive class implemention
+	 */
+	GResult connect();
     
-    /**
-     * @brief to connect ftp server
-     * @param [in] server_addr : server address
-     * @return G_YES/G_NO
-     * @note derive class implemention
-     */
-    GResult connect(const IPPortPair& server_addr);
-    
-    /**
-     * @brief send message
-     * @param [in] data : by sent data
-     * @param [in] len : data length
-     * @return have sent size, -1 failed
-     * @note derive class implemention
-     */
-    GInt64 sendMsg(const GInt8* data, const GUint64 len);
-    
-    /**
-     * @brief message loop handle, new thread
-     * @note derive class implemention
-     */
-    void msgLoop();	
+	/**
+	 * @brief to connect ftp server
+	 * @param [in] server_addr : server address
+	 * @return G_YES/G_NO
+	 * @note derive class implemention
+	 */
+	GResult connect(const IPPortPair& server_addr);
+	
+	/**
+	 * @brief send message
+	 * @param [in] data : by sent data
+	 * @param [in] len : data length
+	 * @return have sent size, -1 failed
+	 * @note derive class implemention
+	 */
+	GInt64 sendMsg(const GInt8* data, const GUint64 len);
+	
+	/**
+	 * @brief message loop handle, new thread
+	 * @note derive class implemention
+	 */
+	void msgLoop();	
 	
 private:
 	TcpClient	m_tcpClient;
