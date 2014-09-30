c5432d43c0ab28c2f52f56ab232958b6cd121eab
duye <wangwei2135@gmail.com>
2014-09-30 14:19:48 +0800

    Update g_rpc_client.h

--- module/gcommon/network/inc/g_rpc_client.h
+++ module/gcommon/network/inc/g_rpc_client.h
@@ -18,14 +18,58 @@
 #pragma once
 
 #include <g_system.h>
+#include <g_http_client.h>
 
 namespace gcom {
 
-class RpcClient
+/**
+ * @brief rpc client
+ */
+class RpcClient : public NetworkClient
 {
 public:
 	RpcClient();
-	explicit RpcClient(const IPPortPair& server);
-	~RpcClient();
+	/**
+	 * @brief constructor
+	 * @param [in] server_addr : ftp server address
+	 * @param [in] interface : network card name, defualt is eth0
+	 */
+	explicit RpcClient(const IPPortPair& server_addr, const GInt8* interface = "eth0");
+	virtual ~RpcClient();
+	
+	/**
+	 * @brief to connect ftp server
+	 * @return G_YES/G_NO
+	 * @note derive class implemention
+	 */
+	GResult connect();
+    
+	/**
+	 * @brief to connect ftp server
+	 * @param [in] server_addr : server address
+	 * @return G_YES/G_NO
+	 * @note derive class implemention
+	 */
+	GResult connect(const IPPortPair& server_addr);
+	
+	/**
+	 * @brief send message
+	 * @param [in] data : by sent data
+	 * @param [in] len : data length
+	 * @return have sent size, -1 failed
+	 * @note derive class implemention
+	 */
+	GInt64 sendMsg(const GInt8* data, const GUint64 len);
+	
+	/**
+	 * @brief message loop handle, new thread
+	 * @note derive class implemention
+	 */
+	void msgLoop();	
+	
+private:
+	TcpClient	m_tcpClient;
+	UdpClinet	m_udpClient;
 };
+
 }
