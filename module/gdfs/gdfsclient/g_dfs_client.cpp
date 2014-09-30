a46b4ae111d0b216f7f471d4fd579b1928546e2a
duye <wangwei2135@gmail.com>
2014-09-30 15:32:14 +0800

    Update g_dfs_client.cpp

--- module/gdfs/gdfsclient/g_dfs_client.cpp
+++ module/gdfs/gdfsclient/g_dfs_client.cpp
@@ -14,34 +14,59 @@
 *  1. 2014-09-30 duye Created this file
 * 
 */
+#include <g_system.h>
+#include <g_logger.h>
 #include <g_dfs_client.h>
 
 namespace gdfs {
+    
+static const GInt8* LOG_PREFIX = "dfs.client";
   
 DfsClient::DfsClient() {}
 DfsClient::~DfsClient() {}
 
 GResult DfsClient::start()
 {
+    // start client thread
+    this->startTask();
+    
     return G_YES;    
 }
 
-GResult DfsClient::serverResponse(const GInt8* msg)
+GResult DfsClient::onServerResponse(const GInt8* msg)
 {
+    G_LOG_IN();
     // TODB
     // handle server response
+    G_LOG_OUT();
+    
     return G_YES;
 }
 
 GResult DfsClient::run()
 {
+    G_LOG_IN();
+    
+    // add observer
+    m_rpcClient.addObserver(this);
+    
     // event loop
     while (true)
     {
+        if (m_rpcClient.)
         char* cmd = gets();
         
-        if (cmd)
+        if (cmd == nullptr)
+        {
+            
+        }
+        else if (cmd == )
+        {
+            
+        }
     }
+    
+    G_LOG_OUT();
     
     return G_YES;
 }
