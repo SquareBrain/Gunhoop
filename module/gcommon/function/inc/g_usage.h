ed296299cb182a30d11300c256ca0f6dc4e388a7
duye <wangwei2135@gmail.com>
2014-09-30 17:43:24 +0800

    Create g_usage.h

---
+++ module/gcommon/function/inc/g_usage.h
@@ -0,0 +1,25 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_usage.h
+* @version     
+* @brief      
+* @author	duye
+* @date		2014-09-30
+* @note 
+*
+*  1. 2014-09-30 duye Created this file
+* 
+*/
+#pragma once
+
+#include <g_type.h>
+#include <g_system.h>
+#include <list>
+
+namespace gcom {
+
+}
