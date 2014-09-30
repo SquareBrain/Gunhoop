f64995469770ba6653507f5bf86873226f1ccdfa
duye <wangwei2135@gmail.com>
2014-09-30 17:44:36 +0800

    Create g_usage.cpp

---
+++ module/gcommon/function/src/g_usage.cpp
@@ -0,0 +1,23 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_usage.cpp
+* @version     
+* @brief      
+* @author	duye
+* @date		2014-09-30
+* @note 
+*
+*  1. 2014-09-30 duye Created this file
+* 
+*/
+#pragma once
+
+#include <g_usage.h>
+
+namespace gcom {
+
+}
