4b1b5e7bdd50f0be69b1b55cd0a1f39583e3bc3c
duye <wangwei2135@gmail.com>
2014-09-30 14:08:43 +0800

    Create g_dfs_client.h

---
+++ module/gdfs/gdfsclient/g_dfs_client.h
@@ -0,0 +1,23 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_dfs_client.h
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-09-30
+* @note 
+*
+*  1. 2014-09-30 duye Created this file
+* 
+*/
+#pragma once
+
+#include <g_rpc_client.h>
+
+namespace gdfs {
+
+}
