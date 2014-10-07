63d36d809f5e3c00be5692a53b7d5792144b1284
duye <wangwei_cp@163.com>
2014-10-07 22:24:31 +0800

    modify files

---
+++ module/gcommon/filesys/inc/g_filesys_def.h
@@ -0,0 +1,17 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file		g_filesys_def.h
+* @version     
+* @brief      
+* @author	duye
+* @date		2014-10-07
+* @note 
+*
+*  1. 2014-10-07 duye Created this file
+* 
+*/
+#pragma once
