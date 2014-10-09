39603f99c8a3d2184bd4766d9884fae72e0c717e
duye <wangwei2135@gmail.com>
2014-10-09 14:46:09 +0800

    Update g_network_server.h

--- module/gcommon/network/inc/g_network_server.h
+++ module/gcommon/network/inc/g_network_server.h
@@ -31,47 +31,47 @@ public:
 	 * @param [in] server_addr : ftp server address
 	 * @param [in] net_card : network card for communication, defualt is eth0
 	 */    
-    explicit NetworkServer(const IPPortPair& server_addr, const std::string& net_card = "eth0");
+	explicit NetworkServer(const IPPortPair& server_addr, const std::string& net_card = "eth0");
 	virtual ~NetworkServer();
 
-    /**
-     * @brief startup service
+	/**
+	 * @brief startup service
 	 * @return G_YES/G_NO
 	 */       
 	virtual GResult start() = 0;
 
-    /**
-     * @brief startup service
+	/**
+	 * @brief startup service
 	 * @param [in] server_addr : ftp server address
 	 * @param [in] net_card : network card for communication, defualt is eth0
 	 * @return G_YES/G_NO
 	 */       
 	virtual GResult start(const IPPortPair& server_addr, const std::string& net_card = "eth0") = 0;
 
-    /**
-     * @brief stop service
-     * @return G_YES/G_NO
-     */
+	/**
+	 * @brief stop service
+	 * @return G_YES/G_NO
+	 */
 	virtual GResult stop() = 0;
 
-    /**
-     * @brief message loop handle, new thread
-     * @note derive class implemention
-     * @return G_YES/G_NO
-     */
-    virtual GResult msgLoop() = 0;    
+	/**
+	 * @brief message loop handle, new thread
+	 * @note derive class implemention
+	 * @return G_YES/G_NO
+	 */
+	virtual GResult msgLoop() = 0;    
 
-    /**
-     * @brief get server address
-     * @return server address
-     */
-    const IPPortPair& getServerAddr() const; 
-
-     /**
-     * @brief get net card
-     * @return net card
-     */
-    const std::string getNetCard() const;    
+	/**
+	 * @brief get server address
+	 * @return server address
+	 */
+	const IPPortPair& getServerAddr() const; 
+	
+	/**
+	 * @brief get net card
+	 * @return net card
+	 */
+	const std::string getNetCard() const;    
 	
 private:	
 	// inherit from base class gsys::ThreadTask
@@ -79,9 +79,8 @@ private:
 	GResult run();
 
 private:
-    // server address
-    IPPortPair  m_serverAddr;
-    std::string m_netCard;
+	IPPortPair  	m_serverAddr;
+	std::string 	m_netCard;
 };
 
 }
