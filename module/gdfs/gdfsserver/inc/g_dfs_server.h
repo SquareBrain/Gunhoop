9efb43ca3785b69c880333b24eb7c507154875c4
duye <wangwei2135@gmail.com>
2014-10-09 15:26:08 +0800

    Update g_dfs_server.h

--- module/gdfs/gdfsserver/inc/g_dfs_server.h
+++ module/gdfs/gdfsserver/inc/g_dfs_server.h
@@ -14,16 +14,11 @@
 *  1. 2014-09-24 duye Created this file
 * 
 */
-
 #pragma once
 
 #include <g_system.h>
 #include <g_common.h>
 #include <g_utils.h>
-
-#include <g_dfs_http_server.h>
-#include <g_dfs_ftp_server.h>
-#include <g_dfs_cli_server.h>
 
 static const std::string DEF_DFS_CFG_FILE_PATH("./dfs_cfg.xml");
 
@@ -32,7 +27,7 @@ namespace gdfs {
 /**
  * @brief dfs server
  */
-class DfsServer : public gcom::NetworkServer, public gcom::Singleton<DfsServer>
+class DfsServer : public gcom::HostServer
 {
 public:
     DfsServer();
@@ -40,36 +35,38 @@ public:
     ~DfsServer();
     
     /**
-     * @brief startup DFF service
-     */
-    GResult start();
-    
+     * @brief startup service
+     * @return G_YES/G_NO
+     * @note inherit from base class gcom::HostServer
+     */       
+    GResult start() = 0;
+	
     /**
-     * @brief stop DFS service
+     * @brief stop service
+     * @return G_YES/G_NO
+     * @note inherit from base class gcom::HostServer
      */
-    GResult stop();
-    
+    GResult stop() = 0;
+	
+private:	
     /**
-     * @brief set/get DFS service state
+     * @brief message loop handle
+     * @return G_YES/G_NO
+     * @note inherit from base class gcom::HostServer
      */
-    void setState(const DFSServerState state);
-    const DFSServerState& getState() const;
+    GResult routine() = 0;     
     
-private:
-    // inherit from base class gsys::ThreadTask
-    GResult run();
     // read DFS Server configuration file
     GResult loadCfg();
+    
     // create service thread
     GResult createService();
     
 private:
-    std::string         m_dfsCfgFilePath;
-    gcom::CfgMgr        m_cfgMgr;
-    
-    // dfs services list
-    DfsHttpServer       m_httpServer;
-    DfsFtpServer        m_ftpServer;
-    DfsCliServer        m_cliServer;
+    std::string             m_dfsCfgFilePath;
+    gcom::CfgMgr            m_cfgMgr;
+    gcom::NetworkServer*    m_httpServer;
+    gcom::NetworkServer*    m_ftpServer;
+    gcom::NetworkServer*    m_cliServer;
 };
 }
