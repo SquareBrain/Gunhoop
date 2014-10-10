b3f7908a06e443b232b278a70275adcc707c8b9f
duye <wangwei2135@gmail.com>
2014-10-10 17:42:23 +0800

    Update g_server_factory.h

--- module/gcommon/network/inc/g_server_factory.h
+++ module/gcommon/network/inc/g_server_factory.h
@@ -4,14 +4,47 @@
 *
 *************************************************************************************/
 /**
-* @file		g_server_factory.h
+* @file	    g_server_factory.h
 * @version     
 * @brief      
-* @author	duye
-* @date		2014-10-10
+* @author   duye
+* @date	    2014-10-10
 * @note 
 *
 *  1. 2014-10-10 duye Created this file
 * 
 */
 #pragma once
+
+#include <g_network_server.h>
+
+namespace gcom {
+    
+/**
+ * @brief server type
+ */
+typedef enum
+{
+    SERVER_UNKNOWN = 0,
+    SERVER_FTP,
+    SERVER_HTTP,
+    SERVER_RPC,
+    SERVER_TCP,
+    SERVER_UDP,
+    SERVER_CLI
+} ServerType;
+    
+/**
+ * @brief server factory
+ */
+class ServerFactory
+{
+public:
+    ServerFactory();
+    virtual ~ServerFactory();
+    
+    virtual void createServer(const ServerType& server_type) = 0;
+    virtual void destroyServer(NetworkServer* server) = 0;
+};
+
+}
