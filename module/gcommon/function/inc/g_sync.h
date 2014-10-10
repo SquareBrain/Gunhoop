f7ec0062521c60cbd63419fd6c110e7931e328f1
duye <wangwei2135@gmail.com>
2014-10-10 16:26:17 +0800

    Update g_sync.h

--- module/gcommon/function/inc/g_sync.h
+++ module/gcommon/function/inc/g_sync.h
@@ -39,15 +39,29 @@ public:
      * @brief auto lock
      * @param [in] auto_lock : auto lock object
      */
-	void autoLock(const gsys::AutoLock& auto_lock);   
+    void lock();   
+    void unlock();
+    gsys::Mutex& mutex() const;
     
 private:
     gsys::Mutex	m_mutex;
 };
 
-template <class T> void SecrityObj::autoLock(const gsys::AutoLock& auto_lock)
+template <class T> 
+void SecrityObj::lock()
 {
-	auto_lock.
+	m_mutex.lock();
 }
 
+template <class T>
+void SecrityObj::unlock()
+{
+	m_mutex.unlock();
+}
+
+template <class T>
+gsys::Mutex Secrity::mutex() const
+{
+	return m_mutex;
+}
 }
