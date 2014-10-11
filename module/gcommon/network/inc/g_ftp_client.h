0397f40fbcab8b46f8ab8d90f264963ceb36e229
duye <wangwei2135@gmail.com>
2014-10-11 11:16:19 +0800

    Update g_ftp_client.h

--- module/gcommon/network/inc/g_ftp_client.h
+++ module/gcommon/network/inc/g_ftp_client.h
@@ -4,7 +4,7 @@
 *
 *************************************************************************************/
 /**
-* @file		g_ftp_client.h
+* @file	    g_ftp_client.h
 * @version     
 * @brief      
 * @author   duye
@@ -23,40 +23,41 @@ namespace gcom {
 class FtpClient : public NetworkClient
 {
 public:
-	FtpClient();
-	/**
-	 * @brief constructor
-	 * @param [in] server_addr : ftp server address
-	 * @param [in] net_card : network card for communication, defualt is eth0
-	 */
-	explicit FtpClient(const IPPortPair& server_addr, const std::string& net_card = "eth0");
-	virtual ~FtpClient();
+    FtpClient();
+    
+    /**
+     * @brief constructor
+     * @param [in] server_addr : ftp server address
+     * @param [in] net_card : network card for communication, defualt is eth0
+     */
+    explicit FtpClient(const IPPortPair& server_addr, const std::string& net_card = "eth0");
+    virtual ~FtpClient();
 	
-	/**
-	 * @brief to connect ftp server
-	 * @return G_YES/G_NO
-	 * @note derive class implemention
-	 */
-	GResult connect();
+    /**
+     * @brief to connect ftp server
+     * @return G_YES/G_NO
+     * @note derive class implemention
+     */
+    GResult connect();
     
     /**
      * @brief to connect ftp server
      * @param [in] server_addr : server address
-	 * @param [in] server_addr : ftp server address
-	 * @param [in] net_card : network card for communication, defualt is eth0     
+     * @param [in] server_addr : ftp server address
+     * @param [in] net_card : network card for communication, defualt is eth0     
      * @return G_YES/G_NO
      * @note derive class implemention
      */
     GResult connect(const IPPortPair& server_addr, const std::string& net_card = "eth0");
 	
-	/**
-	 * @brief send message
-	 * @param [in] data : by sent data
-	 * @param [in] len : data length
-	 * @return have sent size, -1 failed
-	 * @note derive class implemention
-	 */
-	GInt64 sendMsg(const GInt8* data, const GUint64 len);
+    /**
+     * @brief send message
+     * @param [in] data : by sent data
+     * @param [in] len : data length
+     * @return have sent size, -1 failed
+     * @note derive class implemention
+     */
+    GInt64 sendMsg(const GInt8* data, const GUint64 len);
 	
     /**
      * @brief message loop handle, new thread
@@ -66,8 +67,8 @@ public:
     virtual GResult msgLoop();	
 	
 private:
-	NetworkClient*	m_tcpClient;
-	NetworkClient*	m_udpClient;
+    NetworkClient*	m_tcpClient;
+    NetworkClient*	m_udpClient;
 };
 
 }
