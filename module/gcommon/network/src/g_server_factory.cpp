8ecd45cf2d9777935ee3a5d5bdb4aa2635f183d3
duye <wangwei2135@gmail.com>
2014-10-12 16:33:45 +0800

    Create g_server_factory.cpp

---
+++ module/gcommon/network/src/g_server_factory.cpp
@@ -0,0 +1,17 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file	    g_server_factory.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date	    2014-10-12
+* @note 
+*
+*  1. 2014-10-12 duye Created this file
+* 
+*/
+#include <g_server_factory.h>
