022a470d01bd97dbf8d994a945a093a9c6266d5f
duye <wangwei2135@gmail.com>
2014-10-12 16:47:10 +0800

    Update g_server_factory.cpp

--- module/gcommon/network/src/g_server_factory.cpp
+++ module/gcommon/network/src/g_server_factory.cpp
@@ -15,3 +15,82 @@
 * 
 */
 #include <g_server_factory.h>
+
+ServerFactory::ServerFactory() {}
+ServerFactory::~ServerFactory() {}
+
+NetworkServer* ServerFactory::createServer(const ServerType& server_type)
+{
+    NetworkServer* server = nullptr;
+    
+    switch (server_type)
+    {
+        case SERVER_FTP:
+            server = new FtpServer;
+            break;
+        case SERVER_HTTP:
+            server = new HttpServer;
+            break;
+        case SERVER_RPC:
+            server = new RpcServer;
+            break;
+        case SERVER_TCP:
+            server = new TcpServer;
+            break;
+        case SERVER_UDP:
+            server = new UdpServer;
+            break;
+        case SERVER_CLI:
+            server = new CliServer;
+            break;
+        default:
+            break;
+    }
+    
+    return server;
+}
+
+void ServerFactory::destroyServer(const ServerType& server_type, NetworkServer* server)
+{
+    switch (server_type)
+    {
+        case SERVER_FTP:
+        {    
+            FtpServer* del_server = dynamic_cast<FtpServer*>server;
+            delete del_server;
+            break;
+        }
+        case SERVER_HTTP:
+        {    
+            HttpServer* del_server = dynamic_cast<HttpServer*>server;
+            delete del_server;
+            break;
+        }
+        case SERVER_RPC:
+        {    
+            RpcServer* del_server = dynamic_cast<RpcServer*>server;
+            delete del_server;
+            break;
+        }
+        case SERVER_TCP:
+        {    
+            TcpServer* del_server = dynamic_cast<TcpServer*>server;
+            delete del_server;
+            break;
+        }
+        case SERVER_UDP:
+        {    
+            UdpServer* del_server = dynamic_cast<UdpServer*>server;
+            delete del_server;
+            break;
+        }
+        case SERVER_CLI:
+        {    
+            CliServer* del_server = dynamic_cast<CliServer*>server;
+            delete del_server;
+            break;
+        }
+        default:
+            break;
+    }    
+}
