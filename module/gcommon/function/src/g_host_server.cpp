1313784e7e6759cf4de3206162a80cf2aa0e35d3
duye <wangwei2135@gmail.com>
2014-10-13 14:55:59 +0800

    Update g_host_server.cpp

--- module/gcommon/function/src/g_host_server.cpp
+++ module/gcommon/function/src/g_host_server.cpp
@@ -15,3 +15,21 @@
 * 
 */
 #include <g_host_server.h>
+
+HostServer::HostServer() : m_serverState(G_HSERVER_INIT) {}
+HostServer::~HostServer() {}
+
+void HostServer::setState(const HostServerState& state) 
+{ 
+    m_state = state; 
+}
+
+const HostServerState& HostServer::state() const 
+{ 
+    return m_state; 
+}
+
+GResult HostServer::run() 
+{ 
+    return routine(); 
+}	
