91a80f176072cae236f98983cb0579ce782ddc5a
duye <wangwei2135@gmail.com>
2014-10-13 14:54:23 +0800

    Create g_host_server.cpp

---
+++ module/gcommon/function/src/g_host_server.cpp
@@ -0,0 +1,17 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file     g_host_server.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-10-13
+* @note 
+*
+*  1. 2014-10-13 duye Created this file
+* 
+*/
+#include <g_host_server.h>
