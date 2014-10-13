ee823396e6122aff89afe36f2c1f4b1cb25a6675
duye <wangwei2135@gmail.com>
2014-10-13 14:57:57 +0800

    Create g_msg_queue.h

---
+++ module/gcommon/function/inc/g_msg_queue.h
@@ -0,0 +1,19 @@
+/************************************************************************************
+**  
+* @copyright (c) 2013-2100, ChengDu Duyer Technology Co., LTD. All Right Reserved.
+*
+*************************************************************************************/
+/**
+* @file     g_msg_queue.cpp
+* @version     
+* @brief      
+* @author   duye
+* @date     2014-10-13
+* @note 
+*
+*  1. 2014-10-13 duye Created this file
+* 
+*/
+#pragma once
+
+#include <g_system.h>
