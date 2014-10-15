4d64b06e17e8f9ad8f48daad1f06820e3fdf2936
duye <wangwei2135@gmail.com>
2014-10-15 16:19:24 +0800

    Update g_condition.h

--- module/gsystem/inc/g_condition.h
+++ module/gsystem/inc/g_condition.h
@@ -29,57 +29,56 @@ namespace gsys {
 class Condition 
 {
 public:
-	Condition();
-	~Condition();
+    Condition();
+    ~Condition();
 
-	/**
-	 * @brief wake up the waitting thread, only wake up one thread,
-	 * according to the thread priority and waitting time.
-	 * @return true/false
-	 * @note 
-	 */ 
-	bool signal();
+    /**
+     * @brief wake up the waitting thread, only wake up one thread,
+     * according to the thread priority and waitting time.
+     * @return true/false
+     * @note 
+     */ 
+    bool signal();
 	 
-	/**
-	 * @brief wake up all waitting threads
-	 * @return true/false
-	 * @note 
-	 */		
-	bool broadcast();
+    /**
+     * @brief wake up all waitting threads
+     * @return true/false
+     * @note 
+     */		
+    bool broadcast();
 
-	/**
-	 * @brief let current thread has reached the awaited state, utill be wake up
-	 * @return true/false
-	 * @note 
-	 */		
-	bool wait();
+    /**
+     * @brief let current thread has reached the awaited state, utill be wake up
+     * @return true/false
+     * @note 
+     */		
+    bool wait();
 
-	/**
-	 * @brief let current thread has reached the awaited state,
-	 * utill be wake up or timeout return
-	 * @param [in] timeout : waitting timeout
-	 * @return true/false
-	 * @note 
-	 */	
-	bool wait(const GUint32 timeout);
+    /**
+     * @brief let current thread has reached the awaited state,
+     * utill be wake up or timeout return
+     * @param [in] timeout : waitting timeout
+     * @return true/false
+     * @note 
+     */	
+    bool wait(const GUint32 timeout);
 
 private: 
-	/**
-	 * @brief for to prevent copying
-	 * @param
-	 * @return true/false
-	 * @note 
-	 */		
-	Condition(const Condition&); 
+    /**
+     * @brief for to prevent copying
+     * @return true/false
+     * @note 
+     */		
+    Condition(const Condition&); 
 
-	/**
-	 * @brief prevent copying
-	 * @note
-	 */	
-	void operator=(const Condition&);	
+    /**
+     * @brief prevent copying
+     * @note
+     */	
+    void operator=(const Condition&);	
     
 private:
-	pthread_cond_t		m_condition;
-	pthread_mutex_t		m_mutex;	
+    pthread_cond_t    m_condition;
+    pthread_mutex_t   m_mutex;	
 };
 }
