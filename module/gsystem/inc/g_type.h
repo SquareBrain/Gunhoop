d3abd25cafc37eed5564db487eee72e7f3f459d7
duye <wangwei2135@gmail.com>
2014-10-15 22:37:22 +0800

    Update g_type.h

--- module/gsystem/inc/g_type.h
+++ module/gsystem/inc/g_type.h
@@ -4,11 +4,11 @@
 *
 *************************************************************************************/
 /**
-* @file		g_type.h
+* @file	    g_type.h
 * @version  	   
 * @brief      
-* @author	duye
-* @date		2014-07-12
+* @author   duye
+* @date	    2014-07-12
 * @note 
 *
 *  1. 2014-07-12 duye Created this file
@@ -21,15 +21,15 @@
 /** 
  * @brief base type define
  */
-typedef char				GInt8;
-typedef short				GInt16;
-typedef int					GInt32;
-typedef long long			GInt64;
-typedef unsigned char		GUint8;
-typedef unsigned short		GUint16;
-typedef unsigned int		GUint32;
-typedef unsigned long long	GUint64;
-typedef int					GResult;
+typedef char                GInt8;
+typedef short		    GInt16;
+typedef int		    GInt32;
+typedef long long	    GInt64;
+typedef unsigned char	    GUint8;
+typedef unsigned short	    GUint16;
+typedef unsigned int	    GUint32;
+typedef unsigned long long  GUint64;
+typedef int		    GResult;
 
 #include <assert.h>
 #include <stddef.h>
@@ -38,19 +38,19 @@ typedef int					GResult;
 /** 
  * @brief assert
  */
-#define G_ASSERT(x)			assert(x)
+#define G_ASSERT(x) assert(x)
 
 /** 
  * @brief limits number for path max length
  */
-#define G_PATH_MAX			512
+#define G_PATH_MAX 512
 
 /** 
  * @brief limits number for file name max length
  */
-#define G_FILE_NAME_MAX		256
+#define G_FILE_NAME_MAX 256
 
 /** 
  * @brief limits number for error info buffer size
  */
-#define G_ERROR_BUF_SIZE	128
+#define G_ERROR_BUF_SIZE 128
