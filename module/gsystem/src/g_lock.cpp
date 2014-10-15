c30913f7f5cd825afd17a3d79700d70a98a16854
duye <wangwei2135@gmail.com>
2014-10-15 22:49:49 +0800

    Update g_lock.cpp

--- module/gsystem/src/g_lock.cpp
+++ module/gsystem/src/g_lock.cpp
@@ -85,7 +85,7 @@ bool TryLock::lock(const GUint32 timeout)
 {
     if (m_mutex.tryLock())
     {
-    	return true;
+        return true;
     }
 
     if (timeout == 0)
@@ -98,19 +98,19 @@ bool TryLock::lock(const GUint32 timeout)
 						
     do
     {
-		if (loops == 1)
-		{
-	  	    usleep(1000 * (timeout % sleepUnit));
-		}
-		else
-		{
-		    usleep(1000 * sleepUnit);    
-		}
+        if (loops == 1)
+        {
+            usleep(1000 * (timeout % sleepUnit));
+	}
+        else
+        {
+            usleep(1000 * sleepUnit);    
+        }
 
-		if (m_mutex.tryLock())
-		{
-		    return true;
-		}
+        if (m_mutex.tryLock())
+        {
+            return true;
+        }
     } while(--loops);
 
     return false;
