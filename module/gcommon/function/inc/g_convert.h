8a446c939613af1d40e843370c27fe7f55b3719e
duye <wangwei2135@gmail.com>
2014-10-15 14:21:33 +0800

    Update g_convert.h

--- module/gcommon/function/inc/g_convert.h
+++ module/gcommon/function/inc/g_convert.h
@@ -4,11 +4,11 @@
 *
 *************************************************************************************/
 /**
-* @file		g_timeconv.h
+* @file	    g_timeconv.h
 * @version     
 * @brief      
-* @author	duye
-* @date		2014-03-09
+* @author   duye
+* @date	    2014-03-09
 * @note 
 *
 *  2. 2014-06-21 duye move to gohoop project 
@@ -29,24 +29,36 @@ namespace gcom {
 class Convert
 {
 public:
-	/**
-	 * @brief split string
-	 * @param [in] src_str : input string
-	 * @param [in] token : split char
-	 * @param [out] split_list : output string list
-	 */
-	static GResult splitString(const std::string& src_str, 
-	    const GInt8 token, 
-	    std::list<std::string>& split_list);
+    /**
+     * @brief split string
+     * @param [in] src_str : input string
+     * @param [in] token : split char
+     * @param [out] split_list : output string list
+     */
+    static GResult splitString(const std::string& src_str, const GInt8 token, std::list<std::string>& split_list);
 
-	/**
-	 * @brief split string
-	 * @param [in] src_str : input string
-	 * @param [in] token : split string
-	 * @param [out] split_list : output string list
-	 */    
-	static GResult splitString(const std::string& src_str, 
-	    const std::string& token, 
-	    std::list<std::string>& split_list);
+    /**
+     * @brief split string
+     * @param [in] src_str : input string
+     * @param [in] token : split string
+     * @param [out] split_list : output string list
+     */    
+    static GResult splitString(const std::string& src_str, const std::string& token, std::list<std::string>& split_list);
+    
+    /**
+     * @brief convert to upper
+     * @param [in/out] ch : input charactor
+     * @param [in/out] str : input string
+     */
+    static void toupper(GInt8& ch);
+    static void toupper(std::string& str);
+    
+    /**
+     * @brief convert to upper
+     * @param [in/out] ch : input charactor
+     * @param [in/out] str : input string
+     */
+    static void tolower(GInt8& ch);
+    static void tolower(std::string& str);    
 };
 }
