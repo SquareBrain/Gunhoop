21c6db67b7c47a139c3132433e474a1122a40ae5
duye <wangwei2135@gmail.com>
2014-10-15 22:28:53 +0800

    Update g_shm.h

--- module/gsystem/inc/g_shm.h
+++ module/gsystem/inc/g_shm.h
@@ -4,18 +4,17 @@
 *
 *************************************************************************************/
 /**
-* @file 	g_shm.h
+* @file     g_shm.h
 * @version     
 * @brief      
-* @author	duye
-* @date		2014-02-22
+* @author   duye
+* @date	    2014-02-22
 * @note 
 *
 *  2. 2014-06-20 duye move to gohoop project 
 *  1. 2014-02-22 duye Created this file
 * 
 */
-
 #pragma once
 
 #include <sys/mman.h>
@@ -32,38 +31,38 @@ namespace gsys {
  */
 typedef enum 
 { 
-	/** 
-	 * @brief open shm failed
-	 */        
-	OPEN_SHM_FAILED = -2,
-	/** 
-	 * @brief munmap failed
-	 */           
-	MUNMAP_SHM_FAILED = -3,
-	/** 
-	 * @brief mmap failed
-	 */           
-	MMAP_SHM_FAILED = -4,
-	/** 
-	 * @brief sync shm failed
-	 */           
-	SYNC_SHM_FAILED = -5,
-	/** 
-	 * @brief write shm parameter failed
-	 */           
-	WRITE_SHM_PARA_FAILED = -6,
-	/** 
-	 * @brief read shm parameter failed
-	 */           
-	READ_SHM_PARA_FAILED = -7,
-	/** 
-	 * @brief shm not init
-	 */           
-	SHM_NO_INIT = -8,
-	/** 
-	 * @brief shm path is empty
-	 */           
-	SHM_PATH_EMPTY = -9
+    /** 
+     * @brief open shm failed
+     */        
+    OPEN_SHM_FAILED = -2,
+    /** 
+     * @brief munmap failed
+     */           
+    MUNMAP_SHM_FAILED = -3,
+    /** 
+     * @brief mmap failed
+     */           
+     MMAP_SHM_FAILED = -4,
+    /** 
+     * @brief sync shm failed
+     */           
+    SYNC_SHM_FAILED = -5,
+    /** 
+     * @brief write shm parameter failed
+     */           
+    WRITE_SHM_PARA_FAILED = -6,
+    /** 
+     * @brief read shm parameter failed
+     */           
+    READ_SHM_PARA_FAILED = -7,
+    /** 
+     * @brief shm not init
+     */           
+    SHM_NO_INIT = -8,
+    /** 
+     * @brief shm path is empty
+     */           
+    SHM_PATH_EMPTY = -9
 } GShmErrorCode;
 
 /** 
@@ -72,77 +71,77 @@ typedef enum
 class Shm
 {
 public:
-	Shm();   
-	/**
-	 * @brief constructor
-	 * @param [in] GShmPath : shm mapping file path
-	 * @param [in] GShmSize : shm size
-	 * @return true/false
-	 * @note 
-	 */		
-	Shm(const GInt8* shmPath, const GUint64 shmSize);
-	~Shm();
+    Shm();   
+    /**
+     * @brief constructor
+     * @param [in] GShmPath : shm mapping file path
+     * @param [in] GShmSize : shm size
+     * @return true/false
+     * @note 
+     */		
+    Shm(const GInt8* shmPath, const GUint64 shmSize);
+    ~Shm();
 
-	/**
-	 * @brief set shm mapping file path
-	 * @param [in] shmPath : shm mapping file path     
-	 * @note 
-	 */	
-	void setPath(const GInt8* shmPath);
+    /**
+     * @brief set shm mapping file path
+     * @param [in] shmPath : shm mapping file path     
+     * @note 
+     */	
+    void setPath(const GInt8* shmPath);
 
-	/**
-	 * @brief set shm mapping file size
-	 * @param [in] shmSize : shm size  
-	 * @note 
-	 */		
-	void setSize(const GUint64 shmSize);
+    /**
+     * @brief set shm mapping file size
+     * @param [in] shmSize : shm size  
+     * @note 
+     */		
+    void setSize(const GUint64 shmSize);
 
-	/**
-	 * @brief sync the shm
-	 * @return G_YES/G_NO  
-	 * @note 
-	 */	
-	GResult sync();
+    /**
+     * @brief sync the shm
+     * @return G_YES/G_NO  
+     * @note 
+     */	
+    GResult sync();
 
-	/**
-	 * @brief write data to shm
-	 * @param [in] offset : offset of shm
-	 * @param [in] data : input data
-	 * @param [in] size : write size     
-	 * @return G_YES/G_NO  
-	 * @note 
-	 */		
-	GResult write(const GUint32 offset, const GInt8* data, const GUint32 length);
+    /**
+     * @brief write data to shm
+     * @param [in] offset : offset of shm
+     * @param [in] data : input data
+     * @param [in] size : write size     
+     * @return G_YES/G_NO  
+     * @note 
+     */		
+    GResult write(const GUint32 offset, const GInt8* data, const GUint32 length);
 
-	/**
-	 * @brief read data from shm
-	 * @param [in] offset : offset of shm
-	 * @param [in] buffer : output buffer
-	 * @param [in] size : read size    
-	 * @return G_YES/G_NO  
-	 * @note 
-	 */	
-	GResult read(const GUint32 offset, GInt8* buffer, const GUint32 size);
-
-private:
-	/**
-	 * @brief init the shm
-	 * @return G_YES/G_NO  
-	 * @note 
-	 */	    	
-	GResult init();
-
-	/**
-	 * @brief uninit the shm
-	 * @return G_YES/G_NO  
-	 * @note 
-	 */	 	
-	GResult uninit();
+    /**
+     * @brief read data from shm
+     * @param [in] offset : offset of shm
+     * @param [in] buffer : output buffer
+     * @param [in] size : read size    
+     * @return G_YES/G_NO  
+     * @note 
+     */	
+    GResult read(const GUint32 offset, GInt8* buffer, const GUint32 size);
 
 private:
-	GInt8	m_shmPath[G_PATH_MAX];
-	GUint64	m_shmSize;	
-	void*	m_shmAddr;
-	bool	m_initFlags;
+    /**
+     * @brief init the shm
+     * @return G_YES/G_NO  
+     * @note 
+     */	    	
+    GResult init();
+
+    /**
+     * @brief uninit the shm
+     * @return G_YES/G_NO  
+     * @note 
+     */	 	
+    GResult uninit();
+
+private:
+    GInt8    m_shmPath[G_PATH_MAX];
+    GUint64  m_shmSize;	
+    void*    m_shmAddr;
+    bool     m_initFlags;
 };
 }
