698614878668483d360b0f8384c0e08cf8f6a7fc
duye <wangwei2135@gmail.com>
2014-10-15 23:03:12 +0800

    Update g_sys.cpp

--- module/gsystem/src/g_sys.cpp
+++ module/gsystem/src/g_sys.cpp
@@ -4,17 +4,16 @@
 *
 *************************************************************************************/
 /**
-* @file		g_sys.cpp
+* @file	    g_sys.cpp
 * @version     
 * @brief      
-* @author	duye
-* @date		2014-03-04
+* @author   duye
+* @date	    2014-03-04
 * @note 
 *
 *  1. 2014-03-04 duye Created this file
 * 
 */
-
 #include <unistd.h>
 #include <stdarg.h>
 #include <sys/time.h>
@@ -26,75 +25,72 @@ namespace gsys {
 
 void System::sleep(const GUint64 time)
 {
-	::sleep(time);
+    ::sleep(time);
 }
 
 void System::usleep(const GUint64 time)
 {
-	::usleep(time);
+    ::usleep(time);
 }
 
 GUint64 System::pformat(GInt8* buffer, const GUint64 size, const GInt8* args, ...)
 {
-	IS_NULL_R(buffer);
-	
-	va_list vaList;
-	va_start(vaList, args);
-	GUint64 strLen = vsnprintf(buffer, size, args, vaList);
-	va_end(vaList);	
-	return strLen;
+    IS_NULL_R(buffer);
+    va_list vaList;
+    va_start(vaList, args);
+    GUint64 strLen = vsnprintf(buffer, size, args, vaList);
+    va_end(vaList);	
+    return strLen;
 }
 
 GResult System::shell(const GInt8* cmd)
 {
-	IS_NULL_R(cmd);
+    IS_NULL_R(cmd);
 
-	FILE* retStream = popen(cmd, "r");
-	if (retStream == NULL)
-	{
-		return G_NO;
-	}
-	
-	pclose(retStream);
+    FILE* retStream = popen(cmd, "r");
+    if (retStream == NULL)
+    {
+    	return G_NO;
+    }
 
-	return G_YES;      
+    pclose(retStream);
+    return G_YES;      
 }
 
 GResult System::shell(const GInt8* cmd, GInt8* buffer, const GUint32 size)
 {
-	IS_NULL_R(cmd);
-	IS_NULL_R(buffer);
+    IS_NULL_R(cmd);
+    IS_NULL_R(buffer);
 
-	FILE* retStream = popen(cmd, "r");
-	if (retStream == NULL)
-	{
-		return G_NO;
-	}
+    FILE* retStream = popen(cmd, "r");
+    if (retStream == NULL)
+    {
+    	return G_NO;
+    }
 
-	memset(buffer, 0, size);
-	fread(buffer, sizeof(GInt8), size, retStream);
+    memset(buffer, 0, size); 
+    fread(buffer, sizeof(GInt8), size, retStream);
 	
-	pclose(retStream);
-
-	return G_YES;      
+    pclose(retStream);
+    return G_YES;      
 }
 
 GUint64 System::sysTime()
 {
-	struct timeval now;
-	struct timezone tz;
-	if (gettimeofday(&now, &tz) < 0)
-	{
-		return 0;
-	}
+    struct timeval now;
+    struct timezone tz;
+    if (gettimeofday(&now, &tz) < 0)
+    {
+    	return 0;
+    }
 	
-	return (GUint64)(now.tv_sec) * 1000000 + now.tv_usec;
+    return (GUint64)(now.tv_sec) * 1000000 + now.tv_usec;
 }
 
 GResult System::sysTime(const GInt8* format, GInt8* buffer, const GUint32 size)
 {
-	IS_NULL_R(format);
-	IS_NULL_R(buffer);
+    IS_NULL_R(format);
+    IS_NULL_R(buffer);
 	
     struct timeval tv;
     struct tm tm;
@@ -115,49 +111,45 @@ GResult System::sysTime(const GInt8* format, GInt8* buffer, const GUint32 size)
 
 GResult System::optArg(GInt32 argc, GInt8** argv, GInt8* cmd, GInt64& value)
 {
-	IS_NULL_R(argv);
-	IS_NULL_R(cmd);
+    IS_NULL_R(argv);
+    IS_NULL_R(cmd);
 	
-	if (getopt(argc, argv, cmd) != cmd[0])
-	{
-		return G_NO;
-	}
+    if (getopt(argc, argv, cmd) != cmd[0])
+    {
+    	return G_NO;
+    }
 	
-	const std::string temp_str(optarg);
-	value = (GInt64)std::stoi(temp_str);
-	
-	return G_YES;
+    const std::string temp_str(optarg);
+    value = (GInt64)std::stoi(temp_str);
+    return G_YES;
 }
 
 GResult System::optArg(GInt32 argc, GInt8** argv, GInt8* cmd, GUint64& value)
 {
-	IS_NULL_R(argv);
-	IS_NULL_R(cmd);
+    IS_NULL_R(argv);
+    IS_NULL_R(cmd);
 	
-	if (getopt(argc, argv, cmd) != cmd[0])
-	{
-		return G_NO;
-	}
-	
-	const std::string temp_str(optarg);
-	value = (GUint64)std::stoi(temp_str);	
-	
-	return G_YES;
+    if (getopt(argc, argv, cmd) != cmd[0])
+    {
+    	return G_NO;
+    }
+    
+    const std::string temp_str(optarg);
+    value = (GUint64)std::stoi(temp_str);	
+    return G_YES;
 }
 
 GResult System::optArg(GInt32 argc, GInt8** argv, GInt8* cmd, std::string& value)
 {
-	IS_NULL_R(argv);
-	IS_NULL_R(cmd);
+    IS_NULL_R(argv);
+    IS_NULL_R(cmd);
 	
-	if (getopt(argc, argv, cmd) != cmd[0])
-	{
-		return G_NO;
-	}
+    if (getopt(argc, argv, cmd) != cmd[0])
+    {
+    	return G_NO;
+    }
 	
-	value.assign(optarg);
-	
-	return G_YES;
+    value.assign(optarg);
+    return G_YES;
 }
-
 }
