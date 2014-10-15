ca5c35f6f0e512996c6a9437e5dd904a368b5d98
duye <wangwei2135@gmail.com>
2014-10-15 22:39:24 +0800

    Update g_condition.cpp

--- module/gsystem/src/g_condition.cpp
+++ module/gsystem/src/g_condition.cpp
@@ -4,11 +4,11 @@
 *
 *************************************************************************************/
 /**
-* @file		g_condition.cpp
+* @file	    g_condition.cpp
 * @version     
 * @brief      
-* @author	duye
-* @date		2013-06-20
+* @author   duye
+* @date	    2013-06-20
 * @note 
 *
 *  1. 2013-06-20 duye Created this file
@@ -16,60 +16,58 @@
 */
 #include <g_condition.h>
 
-using namespace gsys;
+namespace gsys {
 
 Condition::Condition()       
 { 
-	pthread_cond_init(&m_condition, 0);        
-	pthread_mutex_init(&m_mutex, 0);       
+    pthread_cond_init(&m_condition, 0);        
+    pthread_mutex_init(&m_mutex, 0);       
 }       
 
 Condition::~Condition()     
 {      
-	pthread_cond_destroy(&m_condition);        
-	pthread_mutex_destroy(&m_mutex);    
+    pthread_cond_destroy(&m_condition);        
+    pthread_mutex_destroy(&m_mutex);    
 }
 
 bool Condition::signal()        
 {        
-	pthread_mutex_lock(&m_mutex);
-	GInt32 ret = pthread_cond_signal(&m_condition);
-	pthread_mutex_unlock(&m_mutex);
-
-	return (ret == 0 ? true : false);
+    pthread_mutex_lock(&m_mutex);
+    GInt32 ret = pthread_cond_signal(&m_condition);
+    pthread_mutex_unlock(&m_mutex);
+    return (ret == 0 ? true : false);
 }    
 
 bool Condition::broadcast() 
 {     
-	pthread_mutex_lock(&m_mutex);
-	GInt32 ret = pthread_cond_broadcast(&m_condition);
-	pthread_mutex_unlock(&m_mutex);
-
-	return (ret == 0 ? true : false);
+    pthread_mutex_lock(&m_mutex);
+    GInt32 ret = pthread_cond_broadcast(&m_condition);
+    pthread_mutex_unlock(&m_mutex);
+    return (ret == 0 ? true : false);
 }     
 
 bool Condition::wait()  
 {        
-	pthread_mutex_lock(&m_mutex);
-	GInt32 ret = pthread_cond_wait(&m_condition, &m_mutex);
-	pthread_mutex_unlock(&m_mutex);
-
-	return (ret == 0 ? true : false);
+    pthread_mutex_lock(&m_mutex);
+    GInt32 ret = pthread_cond_wait(&m_condition, &m_mutex);
+    pthread_mutex_unlock(&m_mutex);
+    return (ret == 0 ? true : false);
 }      
 
 bool Condition::wait(const GUint32 timeout)     
 {       
-	struct timeval now;    
-	struct timespec tmpTimeout;  
+    struct timeval now;    
+    struct timespec tmpTimeout;  
 
-	gettimeofday(&now, 0);     
+    gettimeofday(&now, 0);     
 
-	tmpTimeout.tv_sec = now.tv_sec + timeout / 1000;       
-	tmpTimeout.tv_nsec = now.tv_usec * 1000 + timeout % 1000 * 1000 * 1000;    
+    tmpTimeout.tv_sec = now.tv_sec + timeout / 1000;       
+    tmpTimeout.tv_nsec = now.tv_usec * 1000 + timeout % 1000 * 1000 * 1000;    
 
-	pthread_mutex_lock(&m_mutex);
-	GInt32 ret = pthread_cond_timedwait(&m_condition, &m_mutex, &tmpTimeout);
-	pthread_mutex_unlock(&m_mutex);
+    pthread_mutex_lock(&m_mutex);
+    GInt32 ret = pthread_cond_timedwait(&m_condition, &m_mutex, &tmpTimeout);
+    pthread_mutex_unlock(&m_mutex);
 
-	return (ret == 0 ? true : false);
+    return (ret == 0 ? true : false);
+}
 }
