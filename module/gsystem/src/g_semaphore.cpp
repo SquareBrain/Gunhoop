42a7bb170b65682f32c1c7ae8ed1a729c463d349
duye <wangwei2135@gmail.com>
2014-10-15 09:32:11 +0800

    Update g_semaphore.cpp

--- module/gsystem/src/g_semaphore.cpp
+++ module/gsystem/src/g_semaphore.cpp
@@ -20,38 +20,38 @@ namespace gsys {
 	
 Semaphore::Semaphore()
 {
-	// 2st paramater not equal to zero indicate shared in process
-	// otherwise shared in all thread in current process
-	// 3st paramater is the initialization value of semaphore
-	sem_init(&m_sem, 0, 0);
+    // 2st paramater not equal to zero indicate shared in process
+    // otherwise shared in all thread in current process
+    // 3st paramater is the initialization value of semaphore
+    sem_init(&m_sem, 0, 0);
 }
 
 Semaphore::~Semaphore()
 {
-	sem_destroy(&m_sem);
+    sem_destroy(&m_sem);
 }
 
 bool Semaphore::wait()
 {
-	return sem_wait(&m_sem) == 0 ? true : false;
+    return sem_wait(&m_sem) == 0 ? true : false;
 }
 
 bool Semaphore::tryWait()
 {
-	return sem_trywait(&m_sem) == 0 ? true : false;
+    return sem_trywait(&m_sem) == 0 ? true : false;
 }
 
 bool Semaphore::timedWait(const GUint64 timeout)
 {
-	struct timespec ts;
-	ts.tv_sec += timeout / 1000;
-	ts.tv_nsec += (timeout % 1000) * 1000000;
-	return sem_timedwait(&m_sem, &ts) == 0 ? true : false;	
+    struct timespec ts;
+    ts.tv_sec += timeout / 1000;
+    ts.tv_nsec += (timeout % 1000) * 1000000;
+    return sem_timedwait(&m_sem, &ts) == 0 ? true : false;	
 }
 
 bool Semaphore::post()
 {
-	return sem_post(&m_sem) == 0 ? true : false;
+    return sem_post(&m_sem) == 0 ? true : false;
 }
 
 }
