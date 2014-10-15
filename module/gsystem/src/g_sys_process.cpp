0b61816803dc03243fb537f52948c8b0d0701c2f
duye <wangwei2135@gmail.com>
2014-10-15 11:27:50 +0800

    Update and rename g_process.cpp to g_sys_process.cpp

--- module/gsystem/src/g_sys_process.cpp
+++ module/gsystem/src/g_sys_process.cpp
@@ -4,7 +4,7 @@
 *
 *************************************************************************************/
 /**
-* @file	    g_process.cpp
+* @file	    g_sys_process.cpp
 * @version     
 * @brief      
 * @author   duye
@@ -15,11 +15,39 @@
 * 
 */
 #include <signal.h>
-#include <g_process.h>
+#include <unistd.h>
+#include <g_sys_process.h>
 
 namespace gsys {
 
+static const GUint32 G_DEF_LIMIT_FILE = 1024;
+
 ProcessSysCallbackObserver* ProcessSysCallback::m_observer = nullptr;
+
+SysProcess::SysProcess()
+{
+    setLimitFile(G_DEF_LIMIT_FILE);   	
+}
+
+SysProcess::~SysProcess() {}
+
+GResult SysProcess::setLimitFile(const GUint32 limit_file)
+{
+    m_limitFile = limit_file;
+    struct rlimit rt;
+    rt.rlim_max = rt.rlim_cur = m_limitFile;
+    if (setrlimit(RLIMIT_NOFILE, &rt) == -1) 
+    {
+        return G_NO;
+    }    	
+    
+    return G_YES;
+}
+
+GUint32 SysProcess::limitFile() const
+{
+    return m_limitFile;	
+}
 
 ProcessSysCallback::ProcessSysCallback()
 {
