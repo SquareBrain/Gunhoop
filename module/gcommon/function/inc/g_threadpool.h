09463ff083111bd325e0f8665a0ee9d5f1c43e97
duye <wangwei2135@gmail.com>
2014-10-16 09:57:03 +0800

    Update g_threadpool.h

--- module/gcommon/function/inc/g_threadpool.h
+++ module/gcommon/function/inc/g_threadpool.h
@@ -4,18 +4,17 @@
 *
 *************************************************************************************/
 /**
-* @file		g_threadpool.h
+* @file	    g_threadpool.h
 * @version     
 * @brief      
-* @author	duye
-* @date		2013-12-10
+* @author   duye
+* @date	    2013-12-10
 * @note 
 *
 *  2. 2014-06-21 duye move to gohoop project 
 *  1. 2013-12-10 duye Created this file
 * 
 */
-
 #pragma once
 
 #include <list>
@@ -33,13 +32,13 @@ namespace gcom {
 class ThreadJob
 {
 public:
-	~ThreadJob() {}
+    ~ThreadJob() {}
 
-	/**
-	 * @brief do user work
-	 * @param [in] userData : user data
-	 */  	
-	virtual void work(void* userData) = 0;
+    /**
+     * @brief do user work
+     * @param [in] userData : user data
+     */  	
+    virtual void work(void* userData) = 0;
 };
 
 class ThreadWorker;
@@ -50,75 +49,75 @@ class ThreadWorker;
 class ThreadPool
 {
 public:
-	/**
-	 * @brief thread worker queue
-	 */      
-	typedef std::list<ThreadWorker*> ThreadWorkerList;
+    /**
+     * @brief thread worker queue
+     */      
+    typedef std::list<ThreadWorker*> ThreadWorkerList;
 
 public:
-	ThreadPool();
+    ThreadPool();
 
-	/**
-	 * @brief constructor
-	 * @param [in] threadCount : thread pool count
-	 */    
-	explicit ThreadPool(const GUint32 threadCount);
-	~ThreadPool();
+    /**
+     * @brief constructor
+     * @param [in] threadCount : thread pool count
+     */    
+    explicit ThreadPool(const GUint32 threadCount);
+    ~ThreadPool();
 
-	/**
-	 * @brief start to do user job
-	 * @param [in] threadJob : user job object
-	 * @param [in] userData : user data
-	 * @return G_YES/G_NO
-	 */       
-	GResult doJob(ThreadJob* threadJob, void* userData);
+    /**
+     * @brief start to do user job
+     * @param [in] threadJob : user job object
+     * @param [in] userData : user data
+     * @return G_YES/G_NO
+     */       
+    GResult doJob(ThreadJob* threadJob, void* userData);
 
-	/**
-	 * @brief get thread count
-	 * @param [in] threadJob : user job object
-	 * @param [in] userData : user data
-	 * @return the count of thread pool
-	 */ 	
-	GUint32 getThreadCount() const;
-
-private:
-	/**
-	 * @brief prevent copying
-	 */     
-	ThreadPool(const ThreadPool&);
-	void operator=(const ThreadPool&);
-
-	/**
-	 * @brief initialize thread pool
-	 * @return G_YES/G_NO
-	 */       
-	GResult initThreadPool();
-
-	/**
-	 * @brief uninitialize thread pool
-	 * @return G_YES/G_NO
-	 */  	
-	GResult uninitThreadPool();
-
-	/**
-	 * @brief move idle thread worker from busy queue to idle queue
-	 * @param [in] workerId : thread worker ID
-	 */ 	
-	void moveToIdleList(const GUint32 workerId);
-
-	/**
-	 * @brief move busy thread worker from idle queue to busy queue
-	 * @param [in]workerId : thread worker ID
-	 */ 	
-	void moveToBusyList(const GUint32 workerId);
+    /**
+     * @brief get thread count
+     * @param [in] threadJob : user job object
+     * @param [in] userData : user data
+     * @return the count of thread pool
+     */ 	
+    GUint32 getThreadCount() const;
 
 private:
-	// thread pool size
-	GUint32				m_threadCount;
-	// the idle queue
-	ThreadWorkerList	m_idleThreadWorkerList;
-	// the busy queue
-	ThreadWorkerList	m_busyThreadWorkerList;
+    /**
+     * @brief prevent copying
+     */     
+    ThreadPool(const ThreadPool&);
+    void operator=(const ThreadPool&);
+
+    /**
+     * @brief initialize thread pool
+     * @return G_YES/G_NO
+     */       
+    GResult initThreadPool();
+
+    /**
+     * @brief uninitialize thread pool
+     * @return G_YES/G_NO
+     */  	
+    GResult uninitThreadPool();
+
+    /**
+     * @brief move idle thread worker from busy queue to idle queue
+     * @param [in] workerId : thread worker ID
+     */ 	
+    void moveToIdleList(const GUint32 workerId);
+
+    /**
+     * @brief move busy thread worker from idle queue to busy queue
+     * @param [in]workerId : thread worker ID
+     */ 	
+    void moveToBusyList(const GUint32 workerId);
+
+private:
+    // thread pool size
+    GUint32             m_threadCount;
+    // the idle queue
+    ThreadWorkerList    m_idleThreadWorkerList;
+    // the busy queue
+    ThreadWorkerList    m_busyThreadWorkerList;
 };
 
 /** 
@@ -127,50 +126,50 @@ private:
 class ThreadWorker : public ThreadTask
 {
 public:
-	/**
-	 * @brief constructor
-	 * @param [in] workerId : worker ID
-	 */       
-	explicit ThreadWorker(const GUint32 workerId);
-	virtual ~ThreadWorker();
+    /**
+     * @brief constructor
+     * @param [in] workerId : worker ID
+     */       
+    explicit ThreadWorker(const GUint32 workerId);
+    virtual ~ThreadWorker();
 
-	/**
-	 * @brief get thread worker ID
-	 * @param [in] workerId : worker ID
-	 * @return thread worker ID
-	 */     
-	GUint32 getWorkerId() const;
+    /**
+     * @brief get thread worker ID
+     * @param [in] workerId : worker ID
+     * @return thread worker ID
+     */     
+    GUint32 getWorkerId() const;
 
-	/**
-	 * @brief to do work
-	 * @param [in] threadJob : user thread job
-	 * @param [in] userData : user data
-	 * @return G_YES/G_NO
-	 */  	
-	GResult doWork(ThreadJob* threadJob, void* userData);		
-
-private:
-	/**
-	 * @brief prevent copying
-	 */     
-	ThreadWorker(const ThreadWorker&);
-	void operator=(const ThreadWorker&);
-
-	/**
-	 * @brief thread entry function
-	 * @return G_YES/G_NO
-	 * @note 
-	 */	
-	GResult run();
+    /**
+     * @brief to do work
+     * @param [in] threadJob : user thread job
+     * @param [in] userData : user data
+     * @return G_YES/G_NO
+     */  	
+    GResult doWork(ThreadJob* threadJob, void* userData);		
 
 private:
-	// thread worker ID, setting by external
-	GUint32			m_workerId;
-	// waitting condition
-	Condition		m_condition;
-	// current running job
-	ThreadJob*		m_threadJob;	
-	// current running user data
-	void*			m_userData;	
+    /**
+     * @brief prevent copying
+     */     
+    ThreadWorker(const ThreadWorker&);
+    void operator=(const ThreadWorker&);
+
+    /**
+     * @brief thread entry function
+     * @return G_YES/G_NO
+     * @note 
+     */	
+    GResult run();
+
+private:
+    // thread worker ID, setting by external
+    GUint32       m_workerId;
+    // waitting condition
+    Condition     m_condition;
+    // current running job
+    ThreadJob*    m_threadJob;	
+    // current running user data
+    void*         m_userData;	
 };
 }
