ffdd2187023cace80d66cd832a5413e93d74b6fa
duye <wangwei2135@gmail.com>
2014-10-16 09:51:59 +0800

    Update g_objpool.h

--- module/gcommon/function/inc/g_objpool.h
+++ module/gcommon/function/inc/g_objpool.h
@@ -4,17 +4,16 @@
 *
 *************************************************************************************/
 /**
-* @file		g_objpool.h
+* @file	    g_objpool.h
 * @version     
 * @brief      
-* @author	duye
-* @date		2014-06-18
+* @author   duye
+* @date	    2014-06-18
 * @note 
 *
 *  1. 2014-06-18 duye Created this file
 * 
 */
-
 #pragma once
 
 #include <list>
@@ -28,19 +27,19 @@ namespace gcom {
 class ObjPoolIf
 {
 public:
-	virtual ~ObjPoolIf() {}
+    virtual ~ObjPoolIf() {}
 
-	/**
-	 * @brief be call by object pool, before used object
-	 * @return G_YES/G_NO
-	 */    
-	virtual GResult init() = 0;
+    /**
+     * @brief be call by object pool, before used object
+     * @return G_YES/G_NO
+     */    
+    virtual GResult init() = 0;
 
-	/**
-	 * @brief be call by object pool, after using finished
-	 * @return G_YES/G_NO
-	 */   
-	virtual GResult uninit() = 0;
+    /**
+     * @brief be call by object pool, after using finished
+     * @return G_YES/G_NO
+     */   
+    virtual GResult uninit() = 0;
 };
 
 /** 
@@ -50,97 +49,96 @@ template <class T>
 class ObjPool
 {
 public:
-	typedef std::list<T*> ObjList;
+    typedef std::list<T*> ObjList;
 
 public:
-	ObjPool();
-	/**
-	 * @brief constructor
-	 * @param count : the size of object pool
-	 */    
-	ObjPool(const GUint32 count);
-	~ObjPool();
+    ObjPool();
+    
+    /**
+     * @brief constructor
+     * @param count : the size of object pool
+     */    
+    ObjPool(const GUint32 count);
+    ~ObjPool();
 
-	/**
-	 * @brief set the size of object pool
-	 * @param count : the size of object pool
-	 */
-	void setCount(const GUint32 count);
+    /**
+     * @brief set the size of object pool
+     * @param count : the size of object pool
+     */
+    void setCount(const GUint32 count);
 
-	/**
-	 * @brief get the size of object pool
-	 * @return count : the size of object pool
-	 */
-	GUint32 getCount() const;
+    /**
+     * @brief get the size of object pool
+     * @return count : the size of object pool
+     */
+    GUint32 getCount() const;
 
 private:
-	ObjPool(const ObjPool<T>&);
-	ObjPool<T>& operator=(const ObjPool<T>&); 
+    ObjPool(const ObjPool<T>&);
+    ObjPool<T>& operator=(const ObjPool<T>&); 
 
-	void initObjPool();
-	void releaseObjPool();
+    void initObjPool();
+    void releaseObjPool();
     
 private:    
-	GUint32     m_count;
-	ObjList     m_used;
-	ObjList     m_idle;
+    GUint32     m_count;
+    ObjList     m_used;
+    ObjList     m_idle;
 };
 
 template <class T>
-ObjPool<T>::ObjPool() : m_count(0)
-{
-}
+ObjPool<T>::ObjPool() : m_count(0) {}
 
 template <class T>
 ObjPool<T>::ObjPool(const GUint32 count) : m_count(count)
 {
-	initObjPool();
+    initObjPool();
 }
 
 template <class T>
 ObjPool<T>::~ObjPool()
 {
-	releaseObjPool();   
+    releaseObjPool();   
 }
 
 template <class T>
 void ObjPool<T>::setCount(const GUint32 count)
 {
-	releaseObjPool();
-	m_count = count;
-	initObjPool();
+    releaseObjPool();
+    m_count = count;
+    initObjPool();
 }
 
 template <class T>
 GUint32 ObjPool<T>::getCount() const
 {
-	return m_count;
+    return m_count;
 }
 
 template <class T>
 void ObjPool<T>::initObjPool()
 {
-	if (m_count == 0)
-	{
-		return;
-	}
+    if (m_count == 0)
+    {
+    	return;
+    }
 
-	for (GUint32 i = 0; i < m_count; i++)
+    for (GUint32 i = 0; i < m_count; i++)
+    {
+    	T* t = new T;
+    	if (t->Init())
 	{
-		T* t = new T;
-		if (t->Init())
-		{
-			m_idle.push_back(new T); 
-		}
-	}
+		m_idle.push_back(new T); 
+        }
+    }
 }
 
 template <class T>
 void ObjPool<T>::releaseObjPool()
 {
-	if (m_count == 0)
-	{
-		return;
-	}
+    if (m_count == 0)
+    {
+    	return;
+    }
 }
 }
