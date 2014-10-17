06413568db59d167f6a0c98722684fef51151c20
duye <wangwei2135@gmail.com>
2014-10-17 09:53:47 +0800

    Update g_xml_error.cpp

--- module/gutils/gxml/src/g_xml_error.cpp
+++ module/gutils/gxml/src/g_xml_error.cpp
@@ -4,19 +4,17 @@
 *
 ************************************************************************************/
 /**
-* @file		g_xml_error.cpp
-* @version     
+* @file	    g_xml_error.cpp
+* @version    
 * @brief      
-* @author	duye
-* @date		2014-8-7
+* @author   duye
+* @date	    2014-08-07
 * @note 
 *
-*  1. 2014-8-7 duye Created this file
+*  1. 2014-08-07 duye Created this file
 * 
 */
 #include <g_xml.h>
-
-using namespace gutils;
 
 // The goal of the seperate error file is to make the first
 // step towards localization. tinyxml (currently) only supports
@@ -29,21 +27,21 @@ namespace gutils {
 
 const char* XmlBase::m_errorString[XmlBase::GXML_ERROR_STRING_COUNT] = 
 {
-	"No error",
-	"Error",
-	"Failed to open file",
-	"Error parsing Element.",
-	"Failed to read Element name",
-	"Error reading Element value.",
-	"Error reading Attributes.",
-	"Error: empty tag.",
-	"Error reading end tag.",
-	"Error parsing Unknown.",
-	"Error parsing Comment.",
-	"Error parsing Declaration.",
-	"Error document empty.",
-	"Error null (0) or unexpected EOF found in input stream.",
-	"Error parsing CDATA.",
-	"Error when XmlDocument added to document, because XmlDocument can only be at the root.",
+    "No error",
+    "Error",
+    "Failed to open file",
+    "Error parsing Element.",
+    "Failed to read Element name",
+    "Error reading Element value.",
+    "Error reading Attributes.",
+    "Error: empty tag.",
+    "Error reading end tag.",
+    "Error parsing Unknown.",
+    "Error parsing Comment.",
+    "Error parsing Declaration.",
+    "Error document empty.",
+    "Error null (0) or unexpected EOF found in input stream.",
+    "Error parsing CDATA.",
+    "Error when XmlDocument added to document, because XmlDocument can only be at the root.",
 };
 }
