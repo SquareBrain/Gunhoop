390aee25523593c7969bc62420cc75e374f9e7fb
duye <wangwei_cp@163.com>
2014-10-25 10:30:36 +0800

    delete files

--- module/code_stat.sh
+++ module/code_stat.sh
@@ -8,3 +8,4 @@ echo "files:" >> code_stat.txt
 ls -lR ${DIR} | grep '.[cpp,h]' | wc -l >> code_stat.txt
 echo "lines:" >> code_stat.txt
 find $DIR -name "*.[cpp,h]*" |xargs cat|grep -v ^$|wc -l >> code_stat.txt
+cat code_stat.txt
